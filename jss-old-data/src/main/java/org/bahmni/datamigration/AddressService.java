fee99393752927246a20930898e51e90977ade68
Vivek Singh <petmongrels@gmail.com>
2013-05-29 01:48:13 +0530

    Added null/empty conditions

--- jss-old-data/src/main/java/org/bahmni/datamigration/AddressService.java
+++ jss-old-data/src/main/java/org/bahmni/datamigration/AddressService.java
@@ -1,5 +1,7 @@
 package org.bahmni.datamigration;
 
+import org.apache.commons.lang.StringUtils;
+
 public class AddressService {
     private MasterTehsils masterTehsils;
     private AmbiguousTehsils ambiguousTehsils;
@@ -12,8 +14,20 @@ public class AddressService {
     }
 
     public FullyQualifiedTehsil getTehsilFor(FullyQualifiedTehsil tehsilFromPatientRecord) {
-        String correctedTehsil = correctedTehsils.correctedTehsil(tehsilFromPatientRecord.getTehsil());
+        String tehsil = tehsilFromPatientRecord.getTehsil();
+        if (StringUtils.isBlank(tehsil)) return tehsilFromPatientRecord;
+
+        String correctedTehsil = correctedTehsils.correctedTehsil(tehsil);
+        if (correctedTehsil == null)
+            return tehsilFromPatientRecord;
+
+        if (StringUtils.isBlank(correctedTehsil))
+            return new FullyQualifiedTehsil("", tehsilFromPatientRecord.getDistrict(), tehsilFromPatientRecord.getState());
+
         FullyQualifiedTehsil matchingMasterTehsil = masterTehsils.getFullyQualifiedTehsil(correctedTehsil);
+        if (matchingMasterTehsil == null)
+            return new FullyQualifiedTehsil(correctedTehsil, tehsilFromPatientRecord.getDistrict(), tehsilFromPatientRecord.getState());
+
         if (ambiguousTehsils.contains(matchingMasterTehsil.getTehsil())) {
             return new FullyQualifiedTehsil(matchingMasterTehsil.getTehsil(),
                     tehsilFromPatientRecord.getDistrict(), tehsilFromPatientRecord.getState());
