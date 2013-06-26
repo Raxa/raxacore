da9f79d67115999d82a8723641471baa1731e8b5
pchandra <pchandra@thoughtworks.com>
2013-06-26 15:50:32 +0530

    Revert "praveen,shruthi|new service for fetching person attribute types"
    This reverts commit 20950142c95140af03675c11a9020bdbc20fe1b6.

--- api/src/main/java/org/bahmni/module/bahmnicore/dao/PersonAttributeTypeDao.java
+++
@@ -1,9 +0,0 @@
-package org.bahmni.module.bahmnicore.dao;
-
-import org.bahmni.module.bahmnicore.model.BahmniPersonAttributeType;
-
-import java.util.List;
-
-public interface PersonAttributeTypeDao {
-	public List<BahmniPersonAttributeType> getAll();
-}
