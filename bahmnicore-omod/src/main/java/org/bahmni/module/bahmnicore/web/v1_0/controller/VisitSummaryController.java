7cf5a8edc516e0de61b2ed0c55244104aff33dcb
pchandra <pchandra@thoughtworks.com>
2013-11-22 14:24:30 +0530

    praveen|fixing wiring up of controller methods

--- bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/VisitSummaryController.java
+++ bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/VisitSummaryController.java
@@ -4,14 +4,17 @@ import org.bahmni.module.bahmnicore.model.VisitSummary;
 import org.openmrs.module.emrapi.visit.EmrVisitService;
 import org.openmrs.module.emrapi.visit.contract.VisitRequest;
 import org.openmrs.module.webservices.rest.web.annotation.WSDoc;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/rest/v1/bahmnicore/visitsummary")
-public class VisitSummaryController {
+public class VisitSummaryController extends BaseRestController {
 
     @Autowired
     EmrVisitService emrVisitService;
@@ -25,7 +28,8 @@ public class VisitSummaryController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/{visitUUID}")
     @WSDoc("Get a summary of the visit")
-    public VisitSummary get(String visitUUID){
+    @ResponseBody
+    public VisitSummary get(@PathVariable("visitUUID")  String visitUUID){
         return new VisitSummary(emrVisitService.find(new VisitRequest(visitUUID)));
     }
 }
