c9bfedc52be04573a385fd1fb359b3460ac982ad
Vinay Venu <vinayv@thoughtworks.com>
2013-12-20 17:07:15 +0530

    DefaultJdbcConnectionProvider provides connection from hibernate session

--- openmrs-elis-atomfeed-client-omod/src/main/java/org/bahmni/module/elisatomfeedclient/api/client/DefaultJdbcConnectionProvider.java
+++ openmrs-elis-atomfeed-client-omod/src/main/java/org/bahmni/module/elisatomfeedclient/api/client/DefaultJdbcConnectionProvider.java
@@ -1,9 +1,13 @@
 package org.bahmni.module.elisatomfeedclient.api.client;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
 import org.ict4h.atomfeed.jdbc.JdbcConnectionProvider;
-import org.openmrs.util.DatabaseUpdater;
+import org.openmrs.api.context.ServiceContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -12,16 +16,26 @@ public class DefaultJdbcConnectionProvider implements JdbcConnectionProvider {
 
     @Override
     public Connection getConnection() throws SQLException {
+        return getSession().connection();
+    }
+
+    private Session getSession() {
+        ServiceContext serviceContext = ServiceContext.getInstance();
+        Class klass = serviceContext.getClass();
         try {
-            return DatabaseUpdater.getConnection();
+            Field field = klass.getDeclaredField("applicationContext");
+            field.setAccessible(true);
+            ApplicationContext applicationContext = (ApplicationContext) field.get(serviceContext);
+            SessionFactory factory = (SessionFactory) applicationContext.getBean("sessionFactory");
+            return factory.getCurrentSession();
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        return null;
     }
 
     @Override
     public void closeConnection(Connection connection) throws SQLException {
-        connection.close();
+        getSession().close();
     }
 }
+
