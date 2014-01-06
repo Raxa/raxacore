f01f7fa7db5bf7d263eed251bbbb1edc7c63ea51
pchandra <pchandra@thoughtworks.com>
2014-01-06 17:30:00 +0530

    Revert "praveen|not doing anything on connection close since the task runner daemon will close the connection"
    This reverts commit 49e2ebbb25cb237fc6134233817415a341ae5eb8.

--- openmrs-elis-atomfeed-client-omod/src/main/java/org/bahmni/module/elisatomfeedclient/api/client/DefaultJdbcConnectionProvider.java
+++ openmrs-elis-atomfeed-client-omod/src/main/java/org/bahmni/module/elisatomfeedclient/api/client/DefaultJdbcConnectionProvider.java
@@ -35,7 +35,7 @@ public class DefaultJdbcConnectionProvider implements JdbcConnectionProvider {
 
     @Override
     public void closeConnection(Connection connection) throws SQLException {
-      //  The daemon will close the connection .Don't do anything
+        getSession().close();
     }
 }
 
