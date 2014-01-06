01433bf2f9e266b3025c861bc86ea0041c4913fd
Vivek Singh <petmongrels@gmail.com>
2014-01-06 12:12:44 +0530

    Fixed database scripts which were mis-created earlier in flyway

--- bahmnicore-omod/src/main/resources/V1_79__EncounterSession_Duration.sql
+++
@@ -1,8 +0,0 @@
-insert into global_property (`property`, `property_value`, `description`, `uuid`)
-values ('bahmni.encountersession.duration',
-			'60',
-			'Encountersession duration in minutes',
-			uuid()
-		);
-
-
