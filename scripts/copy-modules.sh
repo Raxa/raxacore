ef65161920554c72e1d2b8b5fe2b5a03ec44eac2
Vivek Singh <petmongrels@gmail.com>
2014-01-16 14:37:42 +0530

    Added concept words for adtnotes concept. Vivek

---
+++ scripts/copy-modules.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+scp bahmnicore-omod/target/bahmnicore-*-SNAPSHOT.omod root@192.168.33.10:/home/jss/.OpenMRS/modules
+scp openerp-atomfeed-client-omod/target/openerp-atomfeed-client-omod-*.omod root@192.168.33.10:/home/jss/.OpenMRS/modules
