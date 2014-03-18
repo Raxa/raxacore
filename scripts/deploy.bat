f11523f7b77346e7cf8592592a07c82483cf87cd
balajin <balajin@thoughtworks.com>
2014-03-18 15:52:21 +0530

    Balaji, Gurpreet | #1965 | Enhanced the script to ensure that if windows users don't have the ssh key file setup, you will get proper message.

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -13,18 +13,24 @@ set MODULE_DEPLOYMENT_FOLDER=/tmp/deploy_bahmni_core
 set VERSION=4.0-SNAPSHOT
 set CWD=./
 set SCRIPTS_DIR=%CWD%/scripts
+set KEY_FILE=%USERPROFILE%\.vagrant.d\win_insecure_private_key.ppk
 
-REM setup
-putty -ssh vagrant@%MACHINE_IP% -i "%USERPROFILE%\.vagrant.d\win_insecure_private_key.ppk" -m %SCRIPTS_DIR%/setup_environment.sh
-REM Kill tomcat
-putty -ssh vagrant@%MACHINE_IP% -i "%USERPROFILE%\.vagrant.d\win_insecure_private_key.ppk" -m %SCRIPTS_DIR%/tomcat_stop.sh
-REM Deploy Bhamni core
-pscp  %CWD%/bahmnicore-omod/target/bahmnicore-omod-%VERSION%.omod vagrant@%MACHINE_IP%:%MODULE_DEPLOYMENT_FOLDER%
-REM Deploy Open erp atom feed client
-pscp  %CWD%/openerp-atomfeed-client-omod/target/openerp-atomfeed-client-omod-%VERSION%.omod vagrant@%MACHINE_IP%:%MODULE_DEPLOYMENT_FOLDER%
-REM Deploy Open elis
-pscp  %CWD%/openmrs-elis-atomfeed-client-omod/target/elisatomfeedclient-omod-%VERSION%.omod vagrant@%MACHINE_IP%:%MODULE_DEPLOYMENT_FOLDER%
-REM Copy omods into module directories
-putty -ssh vagrant@%MACHINE_IP% -i "%USERPROFILE%\.vagrant.d\win_insecure_private_key.ppk" -m %SCRIPTS_DIR%/deploy_omods.sh
-REM Start tomcat
-putty -ssh vagrant@%MACHINE_IP% -i "%USERPROFILE%\.vagrant.d\win_insecure_private_key.ppk" -m %SCRIPTS_DIR%/tomcat_start.sh
+if exist {%KEY_FILE%} (
+    REM setup
+    putty -ssh vagrant@%MACHINE_IP% -i %KEY_FILE% -m %SCRIPTS_DIR%/setup_environment.sh
+    REM Kill tomcat
+    putty -ssh vagrant@%MACHINE_IP% -i %KEY_FILE% -m %SCRIPTS_DIR%/tomcat_stop.sh
+    REM Deploy Bhamni core
+    pscp  %CWD%/bahmnicore-omod/target/bahmnicore-omod-%VERSION%.omod vagrant@%MACHINE_IP%:%MODULE_DEPLOYMENT_FOLDER%
+    REM Deploy Open erp atom feed client
+    pscp  %CWD%/openerp-atomfeed-client-omod/target/openerp-atomfeed-client-omod-%VERSION%.omod vagrant@%MACHINE_IP%:%MODULE_DEPLOYMENT_FOLDER%
+    REM Deploy Open elis
+    pscp  %CWD%/openmrs-elis-atomfeed-client-omod/target/elisatomfeedclient-omod-%VERSION%.omod vagrant@%MACHINE_IP%:%MODULE_DEPLOYMENT_FOLDER%
+    REM Copy omods into module directories
+    putty -ssh vagrant@%MACHINE_IP% -i %KEY_FILE% -m %SCRIPTS_DIR%/deploy_omods.sh
+    REM Start tomcat
+    putty -ssh vagrant@%MACHINE_IP% -i %KEY_FILE% -m %SCRIPTS_DIR%/tomcat_start.sh
+) else (
+    echo Use puttygen to generate win_insecure_private_key.ppk from your %USERPROFILE%\.vagrant.d\insecure_private_key that comes along with vagrant.
+)
+
