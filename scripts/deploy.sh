17d14bab508cde8ebac15081c8ecfcd2dce8642a
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-03-18 17:48:12 +0530

    Gurpreet, Balaji| #1965 | Unix script to deploy the OMODs automatically to vagrant tomcat and restart

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,20 +4,24 @@
 MODULE_DEPLOYMENT_FOLDER=/tmp/deploy_bahmni_core
 MACHINE_IP=192.168.33.10
 VERSION=4.0-SNAPSHOT
-CWD=./
-SCRIPTS_DIR=$CWD/scripts
+SCRIPTS_DIR=./scripts
+KEY_FILE=~/.vagrant.d/insecure_private_key
 
-#Setup environment
-ssh vagrant@$MACHINE_IP -m $SCRIPTS_DIR/setup_environment.sh
-#Kill tomcat
-ssh vagrant@$MACHINE_IP -m $SCRIPTS_DIR/tomcat_stop.sh
-#Deploy Bhamni core
-scp  $CWD/bahmnicore-omod/target/bahmnicore-omod-$VERSION.omod vagrant@$MACHINE_IP:$MODULE_DEPLOYMENT_FOLDER
-#Deploy Open erp atom feed client
-scp  $CWD/openerp-atomfeed-client-omod/target/openerp-atomfeed-client-omod-$VERSION.omod vagrant@$MACHINE_IP:$MODULE_DEPLOYMENT_FOLDER
-#Deploy Open elis
-scp  $CWD/openmrs-elis-atomfeed-client-omod/target/elisatomfeedclient-omod-$VERSION.omod vagrant@$MACHINE_IP:$MODULE_DEPLOYMENT_FOLDER
-#Copy omods into module directories
-ssh vagrant@$MACHINE_IP -m $SCRIPTS_DIR/deploy_omods.sh
-#Restart tomcat
-ssh vagrant@$MACHINE_IP -m $SCRIPTS_DIR/tomcat_start.sh
+# Setup environment
+ssh vagrant@$MACHINE_IP -i $KEY_FILE < $SCRIPTS_DIR/setup_environment.sh
+
+# Kill tomcat
+ssh vagrant@$MACHINE_IP -i $KEY_FILE < $SCRIPTS_DIR/tomcat_stop.sh
+
+# Deploy Bhamni core
+scp  -i $KEY_FILE ./bahmnicore-omod/target/bahmnicore-omod-$VERSION.omod vagrant@$MACHINE_IP:$MODULE_DEPLOYMENT_FOLDER
+
+# Copy omod files to the vagrant box - in /tmp
+scp -i $KEY_FILE ./openerp-atomfeed-client-omod/target/openerp-atomfeed-client-omod-$VERSION.omod vagrant@$MACHINE_IP:$MODULE_DEPLOYMENT_FOLDER
+scp -i $KEY_FILE ./openmrs-elis-atomfeed-client-omod/target/elisatomfeedclient-omod-$VERSION.omod vagrant@$MACHINE_IP:$MODULE_DEPLOYMENT_FOLDER
+
+#Deploy them from Vagrant /tmp to appropriate location
+ssh vagrant@$MACHINE_IP -i $KEY_FILE < $SCRIPTS_DIR/deploy_omods.sh
+
+# Restart tomcat
+ssh vagrant@$MACHINE_IP -i $KEY_FILE < $SCRIPTS_DIR/tomcat_start.sh
