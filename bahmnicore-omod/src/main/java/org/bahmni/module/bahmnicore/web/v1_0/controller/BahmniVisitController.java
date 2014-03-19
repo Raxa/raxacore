e804eb85e539b876a7d839abc0c1bcbb9f7921c3
arathyjan <arathyjanardhanan@gmail.com>
2014-03-19 15:34:45 +0530

    Bharti, RT  | #1761 | end point for close visit where stop date is null

---
+++ bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/BahmniVisitController.java
@@ -0,0 +1,32 @@
+package org.bahmni.module.bahmnicore.web.v1_0.controller;
+
+import org.openmrs.Visit;
+import org.openmrs.api.VisitService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/bahmnicore/visit")
+public class BahmniVisitController extends BaseRestController {
+
+    private VisitService visitService;
+
+    @Autowired
+    public BahmniVisitController(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "endVisit")
+    @ResponseBody
+    public Visit endVisitNow(@RequestParam(value = "visitId") Integer visitId) {
+        Visit visit = Context.getVisitService().getVisit(visitId);
+        return visitService.endVisit(visit, null);
+    }
+
+
+}
