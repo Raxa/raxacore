9310085c6a12c70a49c1819beb2d4365cd00abe5
balajin <balajin@thoughtworks.com>
2014-03-20 11:16:44 +0530

    Gurptreet, BalajiN | #1965 | One command to build and deploy

--- vagrant-deploy/scripts/vagrant-deploy.sh
+++ vagrant-deploy/scripts/vagrant-deploy.sh
@@ -15,11 +15,11 @@ ssh vagrant@$MACHINE_IP -i $KEY_FILE < $SCRIPTS_DIR/setup_environment.sh
 ssh vagrant@$MACHINE_IP -i $KEY_FILE < $SCRIPTS_DIR/tomcat_stop.sh
 
 # Deploy Bhamni core
-scp  -i $KEY_FILE ./bahmnicore-omod/target/bahmnicore-omod-$VERSION.omod vagrant@$MACHINE_IP:$MODULE_DEPLOYMENT_FOLDER
+scp  -i $KEY_FILE ../bahmnicore-omod/target/bahmnicore-omod-$VERSION.omod vagrant@$MACHINE_IP:$MODULE_DEPLOYMENT_FOLDER
 
 # Copy omod files to the vagrant box - in /tmp
-scp -i $KEY_FILE ./openerp-atomfeed-client-omod/target/openerp-atomfeed-client-omod-$VERSION.omod vagrant@$MACHINE_IP:$MODULE_DEPLOYMENT_FOLDER
-scp -i $KEY_FILE ./openmrs-elis-atomfeed-client-omod/target/elisatomfeedclient-omod-$VERSION.omod vagrant@$MACHINE_IP:$MODULE_DEPLOYMENT_FOLDER
+scp -i $KEY_FILE ../openerp-atomfeed-client-omod/target/openerp-atomfeed-client-omod-$VERSION.omod vagrant@$MACHINE_IP:$MODULE_DEPLOYMENT_FOLDER
+scp -i $KEY_FILE ../openmrs-elis-atomfeed-client-omod/target/elisatomfeedclient-omod-$VERSION.omod vagrant@$MACHINE_IP:$MODULE_DEPLOYMENT_FOLDER
 
 #Deploy them from Vagrant /tmp to appropriate location
 ssh vagrant@$MACHINE_IP -i $KEY_FILE < $SCRIPTS_DIR/deploy_omods.sh
