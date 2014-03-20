6ac65e1e3137419382e13ebee13f0192590a30a8
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-03-21 02:13:37 +0530

    Gurpreet| #1965| Refactored the Vagrant connection into vagrant_functions.sh. Made sure they work independent of OMOD versions.

--- vagrant-deploy/scripts/vagrant/vagrant-deploy.sh
+++ vagrant-deploy/scripts/vagrant/vagrant-deploy.sh
@@ -1,31 +1,29 @@
 #!/bin/sh -x
 
+PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $PATH_OF_CURRENT_SCRIPT/vagrant_functions.sh
+
 #All config is here
 MODULE_DEPLOYMENT_FOLDER=/tmp/deploy_bahmni_core
-MACHINE_IP=192.168.33.10
-VERSION=$2
 CWD=$1
 SCRIPTS_DIR=$CWD/scripts/vagrant
-KEY_FILE=~/.vagrant.d/insecure_private_key
-TIMEOUT="-o ConnectTimeout=5"
-SHELL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_BASE=$SHELL_DIR/../../..
+PROJECT_BASE=$PATH_OF_CURRENT_SCRIPT/../../..
 
 # Setup environment
-ssh vagrant@$MACHINE_IP -i $KEY_FILE $TIMEOUT < $SCRIPTS_DIR/setup_environment.sh
+run_in_vagrant -f "$SCRIPTS_DIR/setup_environment.sh"
 
 # Kill tomcat
-ssh vagrant@$MACHINE_IP -i $KEY_FILE $TIMEOUT < $SCRIPTS_DIR/tomcat_stop.sh
+run_in_vagrant -f "$SCRIPTS_DIR/tomcat_stop.sh"
 
 # Deploy Bhamni core
-scp  -i $KEY_FILE $TIMEOUT $PROJECT_BASE/bahmnicore-omod/target/bahmnicore-omod-$VERSION.omod vagrant@$MACHINE_IP:$MODULE_DEPLOYMENT_FOLDER
+scp_to_vagrant $PROJECT_BASE/bahmnicore-omod/target/bahmnicore*.omod $MODULE_DEPLOYMENT_FOLDER
 
 # Copy omod files to the vagrant box - in /tmp
-scp -i $KEY_FILE $TIMEOUT $PROJECT_BASE/openerp-atomfeed-client-omod/target/openerp-atomfeed-client-omod-$VERSION.omod vagrant@$MACHINE_IP:$MODULE_DEPLOYMENT_FOLDER
-scp -i $KEY_FILE $TIMEOUT $PROJECT_BASE/openmrs-elis-atomfeed-client-omod/target/elisatomfeedclient-omod-$VERSION.omod vagrant@$MACHINE_IP:$MODULE_DEPLOYMENT_FOLDER
+scp_to_vagrant $PROJECT_BASE/openerp-atomfeed-client-omod/target/openerp-atomfeed-client*.omod $MODULE_DEPLOYMENT_FOLDER
+scp_to_vagrant $PROJECT_BASE/openmrs-elis-atomfeed-client-omod/target/elisatomfeedclient*.omod $MODULE_DEPLOYMENT_FOLDER
 
 #Deploy them from Vagrant /tmp to appropriate location
-ssh vagrant@$MACHINE_IP -i $KEY_FILE $TIMEOUT < $SCRIPTS_DIR/deploy_omods.sh
+run_in_vagrant -f "$SCRIPTS_DIR/deploy_omods.sh"
 
 # Restart tomcat
-ssh vagrant@$MACHINE_IP -i $KEY_FILE $TIMEOUT < $SCRIPTS_DIR/tomcat_start.sh
+run_in_vagrant -f "$SCRIPTS_DIR/tomcat_start.sh"
---
+++ vagrant-deploy/scripts/vagrant/vagrant_functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+
+#####################################################################################################
+# This script can be used to call functions which will execute a command in your vagrant box. 
+# -c option will be used to pass a command
+# -f option will be used to pass a full qualified file that contains commands
+#
+# It can also be used to SCP into the vagrant box
+#####################################################################################################
+
+MACHINE_IP=192.168.33.10
+KEY_FILE=~/.vagrant.d/insecure_private_key
+TIMEOUT="-o ConnectTimeout=5"
+
+function run_in_vagrant {
+    
+    if [ "$1" == "-c" ]; then
+		ssh vagrant@$MACHINE_IP -i $KEY_FILE $TIMEOUT "$2"
+	elif [ "$1" == "-f" ]; then
+		ssh vagrant@$MACHINE_IP -i $KEY_FILE $TIMEOUT < "$2"
+	fi
+
+}
+
+# $1: Source  $2: Dest
+function scp_to_vagrant {
+    ssh vagrant@$MACHINE_IP -i $KEY_FILE $TIMEOUT < "$2"
+    scp  -i $KEY_FILE $TIMEOUT $1 vagrant@$MACHINE_IP:$2
+}
