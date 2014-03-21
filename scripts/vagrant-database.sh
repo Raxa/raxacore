a289bb04c37f1ed1793ba31bdb5cf6240a867f99
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-03-21 12:49:20 +0530

    Gurpreet, Balaji|#1965: Added openmrs-core liquibase migrations to vagrant-database script

--- scripts/vagrant-database.sh
+++ scripts/vagrant-database.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -x
-MACHINE_IP=192.168.33.10
-KEY_FILE=~/.vagrant.d/insecure_private_key
-ssh vagrant@$MACHINE_IP -i $KEY_FILE "sudo su - jss -c 'cd /bahmni_temp/ && ./run-modules-liquibase.sh'"
+PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $PATH_OF_CURRENT_SCRIPT/../vagrant-deploy/scripts/vagrant/vagrant_functions.sh
+
+#invoke migration of openmrs core
+run_in_vagrant -c "sudo su - jss -c 'cd /bahmni_temp/ && ./run-liquibase-openmrs.sh'"
+#invoke migrations of bahmni core omods
+run_in_vagrant -c "sudo su - jss -c 'cd /bahmni_temp/ && ./run-modules-liquibase.sh'"
