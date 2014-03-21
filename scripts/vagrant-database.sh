38b0ec9078c98a2525f5b96384a19db92d94bfd2
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-03-21 17:59:36 +0530

    Gurpreet| #1965| Ensure that deploy runs before db runs

--- scripts/vagrant-database.sh
+++ scripts/vagrant-database.sh
@@ -2,6 +2,8 @@
 PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $PATH_OF_CURRENT_SCRIPT/../vagrant-deploy/scripts/vagrant/vagrant_functions.sh
 
+$PATH_OF_CURRENT_SCRIPT/$vagrant-deploy.sh
+
 #invoke migration of openmrs core
 run_in_vagrant -c "sudo su - jss -c 'cd /bahmni_temp/ && ./run-liquibase-openmrs.sh'"
 #invoke migrations of bahmni core omods
