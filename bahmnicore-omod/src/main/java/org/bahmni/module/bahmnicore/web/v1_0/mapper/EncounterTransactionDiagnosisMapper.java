6e64c6db09f744ab4bee19df58801cc086a56966
shruthidipali <shruthidipali@gmail.com>
2014-04-11 16:04:51 +0530

    Shruthi | #2108 | Setting the diagnosis date time to current date time when adding diagnosis

--- bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/mapper/EncounterTransactionDiagnosisMapper.java
+++ bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/mapper/EncounterTransactionDiagnosisMapper.java
@@ -5,12 +5,14 @@ import org.bahmni.module.bahmnicore.contract.encounter.request.BahmniEncounterTr
 import org.openmrs.module.emrapi.encounter.domain.EncounterTransaction;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class EncounterTransactionDiagnosisMapper {
     public void populateDiagnosis(BahmniEncounterTransaction bahmniEncounterTransaction) {
         List<EncounterTransaction.Diagnosis> diagnoses = new ArrayList<>();
         for (BahmniDiagnosis bahmniDiagnosis : bahmniEncounterTransaction.getBahmniDiagnoses()) {
+            bahmniDiagnosis.setDiagnosisDateTime(new Date());
             diagnoses.add(bahmniDiagnosis);
         }
         bahmniEncounterTransaction.setDiagnoses(diagnoses);
