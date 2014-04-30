e0187691985c7049edfa809bf0a10c1348bba708
mujir <mshaikh@thoughtworks.com>
2014-04-30 15:59:43 +0530

    Mujir | adding migration no 81

---
+++ bahmnicore-omod/src/main/resources/V1_81__DeleteConcept.sql
@@ -0,0 +1,12 @@
+CREATE  PROCEDURE delete_concept (name_concept VARCHAR(255))
+BEGIN
+    DECLARE conceptId INT default 0;
+
+    select concept_id INTO conceptId from concept_name where name = name_concept and locale_preferred = 1;
+    delete from concept_set where concept_set = conceptId;
+    delete from concept_word where concept_id = conceptId;
+    delete from concept_name where concept_id = conceptId;
+    delete from concept_numeric where concept_id = conceptId;
+    delete from concept_answer where concept_id = conceptId;
+    delete from concept where concept_id = conceptId;
+END;
