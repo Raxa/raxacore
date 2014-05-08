7776ee9cb8b6f7be71608f7e72f697aae794629a
arathyjan <arathyjanardhanan@gmail.com>
2014-05-08 15:24:56 +0530

    RT | #2007 | beanshell to decide wheather to consume patients from openelis and removing beanshell to be placed in respective config file

--- openmrs-elis-atomfeed-client-omod/src/main/java/org/bahmni/module/elisatomfeedclient/api/worker/OpenElisPatientFeedWorker.java
+++ openmrs-elis-atomfeed-client-omod/src/main/java/org/bahmni/module/elisatomfeedclient/api/worker/OpenElisPatientFeedWorker.java
@@ -1,20 +1,27 @@
 package org.bahmni.module.elisatomfeedclient.api.worker;
 
+import bsh.EvalError;
+import bsh.Interpreter;
 import org.apache.log4j.Logger;
 import org.ict4h.atomfeed.client.domain.Event;
 import org.ict4h.atomfeed.client.service.EventWorker;
+import org.openmrs.util.OpenmrsUtil;
+
+import java.io.IOException;
 
 public class OpenElisPatientFeedWorker implements EventWorker {
     public static final String PATIENT = "patient";
     public static final String ACCESSION = "accession";
     private OpenElisPatientEventWorker patientEventWorker;
     private OpenElisAccessionEventWorker accessionEventWorker;
+    private Interpreter interpreter;
 
     private static Logger logger = Logger.getLogger(OpenElisPatientFeedWorker.class);
 
     public OpenElisPatientFeedWorker(OpenElisPatientEventWorker patientEventWorker, OpenElisAccessionEventWorker accessionEventWorker) {
         this.patientEventWorker = patientEventWorker;
         this.accessionEventWorker = accessionEventWorker;
+        interpreter = new Interpreter();
     }
 
     @Override
@@ -23,13 +30,25 @@ public class OpenElisPatientFeedWorker implements EventWorker {
     }
 
     private EventWorker getEventWorker(Event event) {
-        if (PATIENT.equalsIgnoreCase(event.getTitle())) {
+        Boolean shouldProcessPatientSyn = true;
+
+        try {
+            shouldProcessPatientSyn = (Boolean) interpreter.source(OpenmrsUtil.getApplicationDataDirectory() + "beanshell/open-elis-patient-feed-patient-syn.bsh");
+        } catch (IOException | EvalError error) {
+            logger.info("no file beanshell/open-elis-patient-feed-patient-syn.bsh");
+        }
+
+        if (PATIENT.equalsIgnoreCase(event.getTitle()) && shouldProcessPatientSyn) {
             return patientEventWorker;
         } else if (ACCESSION.equalsIgnoreCase(event.getTitle())) {
             return accessionEventWorker;
         }
 
-        logger.warn(String.format("Could not find a worker for event: %s, details: %s", event.getTitle(), event));
+        if(!shouldProcessPatientSyn) {
+            logger.info("not processing patient feed from openelis");
+        } else {
+            logger.warn(String.format("Could not find a worker for event: %s, details: %s", event.getTitle(), event));
+        }
         return new EmptyEventWorker();
     }
 
