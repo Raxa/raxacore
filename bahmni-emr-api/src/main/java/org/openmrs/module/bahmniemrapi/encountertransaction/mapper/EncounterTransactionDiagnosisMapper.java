93b81e5852ea9423ea15b0d51a982e6db529b402
mihirk <mihir.khatwani@gmail.com>
2014-08-18 13:11:08 +0530

    Mihir | Removing a todo. BahmniDiagnosis setDiagnosisDateTime logic moved to EMRAPI.

--- bahmni-emr-api/src/main/java/org/openmrs/module/bahmniemrapi/encountertransaction/mapper/EncounterTransactionDiagnosisMapper.java
+++ bahmni-emr-api/src/main/java/org/openmrs/module/bahmniemrapi/encountertransaction/mapper/EncounterTransactionDiagnosisMapper.java
@@ -5,15 +5,12 @@ import org.openmrs.module.bahmniemrapi.encountertransaction.contract.BahmniEncou
 import org.openmrs.module.emrapi.encounter.domain.EncounterTransaction;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class EncounterTransactionDiagnosisMapper {
     public void populateDiagnosis(BahmniEncounterTransaction bahmniEncounterTransaction) {
         List<EncounterTransaction.Diagnosis> diagnoses = new ArrayList<>();
         for (BahmniDiagnosis bahmniDiagnosis : bahmniEncounterTransaction.getBahmniDiagnoses()) {
-            // TODO: (Mihir, Mujir) Move to EMR-API
-            bahmniDiagnosis.setDiagnosisDateTime( bahmniDiagnosis.getDiagnosisDateTime() != null ? bahmniDiagnosis.getDiagnosisDateTime() : new Date());
             diagnoses.add(bahmniDiagnosis);
         }
         bahmniEncounterTransaction.setDiagnoses(diagnoses);
