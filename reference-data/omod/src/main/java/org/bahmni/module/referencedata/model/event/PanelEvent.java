044eb8dc7a5c93f0685a37564b0b8df06d072087
mihirk <mihir.khatwani@gmail.com>
2014-09-07 20:24:04 +0530

    Mihir | #653 | Making panels publish tests as well on panel changes

--- reference-data/omod/src/main/java/org/bahmni/module/referencedata/model/event/PanelEvent.java
+++ reference-data/omod/src/main/java/org/bahmni/module/referencedata/model/event/PanelEvent.java
@@ -1,7 +1,14 @@
 package org.bahmni.module.referencedata.model.event;
 
+import org.ict4h.atomfeed.server.service.Event;
+import org.joda.time.DateTime;
 import org.openmrs.Concept;
 import org.openmrs.ConceptClass;
+import org.openmrs.api.context.Context;
+
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.UUID;
 
 public class PanelEvent extends ConceptOperationEvent {
 
@@ -16,5 +23,18 @@ public class PanelEvent extends ConceptOperationEvent {
 
     private boolean isPanelConcept(Concept concept) {
         return concept.getConceptClass() != null && concept.getConceptClass().getUuid().equals(ConceptClass.LABSET_UUID) && isChildOf(concept, TestEvent.TEST_PARENT_CONCEPT_NAME);
+    }
+
+    @Override
+    public Event asAtomFeedEvent(Object[] arguments) throws URISyntaxException {
+        Concept concept = (Concept) arguments[0];
+        String url = String.format(this.url, title, concept.getUuid());
+        List<Concept> setMembers = concept.getSetMembers();
+        for (Concept setMember : setMembers) {
+            if (!isResourceConcept(setMember)) {
+                Context.getConceptService().saveConcept(setMember);
+            }
+        }
+        return new Event(UUID.randomUUID().toString(), title, DateTime.now(), url, url, category);
     }
 }
