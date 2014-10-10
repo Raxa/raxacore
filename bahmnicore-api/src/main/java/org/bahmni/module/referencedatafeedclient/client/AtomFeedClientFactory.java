3e2b0dd3f7d6ae386e8933b6c5f3d0355bd19c2e
rohanpoddar <rohan.poddar@gmail.com>
2014-10-10 11:52:54 +0530

    Rohan, Chethan | #885 | Removing Reference data consume from Bahmni core

--- bahmnicore-api/src/main/java/org/bahmni/module/referencedatafeedclient/client/AtomFeedClientFactory.java
+++
@@ -1,7 +0,0 @@
-package org.bahmni.module.referencedatafeedclient.client;
-
-import org.ict4h.atomfeed.client.service.FeedClient;
-
-public interface AtomFeedClientFactory {
-    FeedClient getAtomFeedClient() throws Exception;
-}
--- bahmnicore-api/src/main/java/org/bahmni/module/referencedatafeedclient/dao/BahmniTestUnitsDao.java
+++
@@ -1,7 +0,0 @@
-package org.bahmni.module.referencedatafeedclient.dao;
-
-import org.bahmni.module.referencedatafeedclient.domain.TestUnitOfMeasure;
-
-public interface BahmniTestUnitsDao {
-    public void updateUnitsForTests(String newUnit, String oldUnit);
-}
