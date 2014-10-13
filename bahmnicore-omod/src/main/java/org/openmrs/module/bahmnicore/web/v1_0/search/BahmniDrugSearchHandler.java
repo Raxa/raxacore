3200237d72725c9bd1606a16283c36d16c84ccf8
Vinkesh Banka <vinkesh.banka@gmail.com>
2014-10-13 17:28:48 +0530

    Banka | #936 | Adding a new drug search handler, to order drugs with name staring with search phrase first.

---
+++ bahmnicore-omod/src/main/java/org/openmrs/module/bahmnicore/web/v1_0/search/BahmniDrugSearchHandler.java
@@ -0,0 +1,47 @@
+package org.openmrs.module.bahmnicore.web.v1_0.search;
+
+import org.openmrs.Drug;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.resource.api.PageableResult;
+import org.openmrs.module.webservices.rest.web.resource.api.SearchConfig;
+import org.openmrs.module.webservices.rest.web.resource.api.SearchHandler;
+import org.openmrs.module.webservices.rest.web.resource.api.SearchQuery;
+import org.openmrs.module.webservices.rest.web.resource.impl.NeedsPaging;
+import org.openmrs.module.webservices.rest.web.response.ResponseException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+@Component
+public class BahmniDrugSearchHandler implements SearchHandler {
+
+    @Override
+    public SearchConfig getSearchConfig() {
+        SearchQuery searchQuery = new SearchQuery.Builder("Allows you to search for drugs").withRequiredParameters("q").build();
+        return new SearchConfig("ordered", RestConstants.VERSION_1 + "/drug", Arrays.asList("1.10.*"), searchQuery);
+    }
+
+    @Override
+    public PageableResult search(RequestContext ctx) throws ResponseException {
+        boolean includeRetired = ctx.getIncludeAll();
+        String searchPhrase = ctx.getParameter("q");
+
+        LinkedHashSet<Drug> drugs = new LinkedHashSet<>(findDrugsStartingWith(searchPhrase, includeRetired, ctx));
+        drugs.addAll(findDrugsContaining(searchPhrase, includeRetired, ctx));
+
+        return new NeedsPaging<>(new ArrayList<>(drugs), ctx);
+    }
+
+    private List<Drug> findDrugsStartingWith(String searchPhrase, boolean includeRetired, RequestContext ctx) {
+        return Context.getConceptService().getDrugs(searchPhrase, null, false, false, includeRetired, ctx.getStartIndex(), ctx.getLimit());
+    }
+
+    private List<Drug> findDrugsContaining(String searchPhrase, boolean includeRetired, RequestContext ctx) {
+        return Context.getConceptService().getDrugs(searchPhrase, null, true, false, includeRetired, ctx.getStartIndex(), ctx.getLimit());
+    }
+}
