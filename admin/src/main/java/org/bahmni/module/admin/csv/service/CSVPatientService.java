7d90377adf786b71ea8cab6f33e4a495a6c1de9b
Mujir <mujir@thoughtworks.com>
2014-11-11 12:22:58 +0530

    Mujir | patient csv import adds name as prefered name

--- admin/src/main/java/org/bahmni/module/admin/csv/service/CSVPatientService.java
+++ admin/src/main/java/org/bahmni/module/admin/csv/service/CSVPatientService.java
@@ -3,19 +3,16 @@ package org.bahmni.module.admin.csv.service;
 import org.apache.commons.lang.StringUtils;
 import org.bahmni.csv.KeyValue;
 import org.bahmni.module.admin.csv.models.PatientRow;
-import org.openmrs.*;
+import org.openmrs.Patient;
+import org.openmrs.PatientIdentifier;
+import org.openmrs.PatientIdentifierType;
+import org.openmrs.PersonAddress;
+import org.openmrs.PersonName;
 import org.openmrs.api.AdministrationService;
 import org.openmrs.api.PatientService;
-import org.openmrs.api.context.Context;
-import org.openmrs.module.addresshierarchy.AddressHierarchyLevel;
-import org.openmrs.module.addresshierarchy.service.AddressHierarchyService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -37,6 +34,7 @@ public class CSVPatientService {
     public Patient save(PatientRow patientRow) throws ParseException {
         Patient patient = new Patient();
         PersonName personName = new PersonName(patientRow.getFirstName(), patientRow.getMiddleName(), patientRow.getLastName());
+        personName.setPreferred(true);
         patient.addName(personName);
 
         if (!StringUtils.isBlank(patientRow.getBirthdate())) {
