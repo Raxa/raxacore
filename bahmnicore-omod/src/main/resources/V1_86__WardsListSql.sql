122d10a3d54f08f29ddebaa8d40afe8a39d091a5
Vikashg <vikashgupta2000@gmail.com>
2015-03-31 15:55:11 +0530

    Vikash, Achinta | #1968 | Putting restriction to voided diagnosis.

--- bahmnicore-omod/src/main/resources/V1_86__WardsListSql.sql
+++ bahmnicore-omod/src/main/resources/V1_86__WardsListSql.sql
@@ -29,7 +29,7 @@ FROM bed_location_map blm
     INNER JOIN person_address pa ON pa.person_id = pv.patient_id
     INNER JOIN (SELECT patient_id, max(encounter_datetime) AS max_encounter_datetime FROM encounter_view WHERE encounter_type_name = 'Admission' GROUP BY patient_id) latestAdmissionEncounter ON pv.patient_id = latestAdmissionEncounter.patient_id
     INNER JOIN encounter_view ev on ev.patient_id = latestAdmissionEncounter.patient_id and ev.encounter_datetime = latestAdmissionEncounter.max_encounter_datetime
-    LEFT OUTER JOIN obs adtNotes on adtNotes.encounter_id = ev.encounter_id and adtNotes.concept_id = (SELECT concept_id from concept_name where name = 'Adt Notes' and concept_name_type = 'FULLY_SPECIFIED')
+    LEFT OUTER JOIN obs adtNotes on adtNotes.encounter_id = ev.encounter_id and adtNotes.voided = 0 and adtNotes.concept_id = (SELECT concept_id from concept_name where name = 'Adt Notes' and concept_name_type = 'FULLY_SPECIFIED')
     LEFT OUTER JOIN encounter_provider ep ON ep.encounter_id = ev.encounter_id
     LEFT OUTER JOIN provider admission_provider ON admission_provider.provider_id = ep.provider_id
     LEFT OUTER JOIN person_name admission_provider_name ON admission_provider_name.person_id = admission_provider.person_id
@@ -41,7 +41,7 @@ FROM bed_location_map blm
             person_name.given_name as providerName
         FROM bed_patient_assignment_map bpam
             INNER JOIN (SELECT person_id, max(obs_id) obs_id from obs where concept_id = (SELECT concept_id from concept_name where name = 'Disposition' and concept_name_type = 'FULLY_SPECIFIED') GROUP BY person_id) maxObsId on  maxObsId.person_id = bpam.patient_id
-            INNER JOIN obs latestDisposition on maxObsId.obs_id = latestDisposition.obs_id
+            INNER JOIN obs latestDisposition on maxObsId.obs_id = latestDisposition.obs_id and latestDisposition.voided = 0
             INNER JOIN concept_name on latestDisposition.value_coded = concept_name.concept_id and concept_name_type = 'FULLY_SPECIFIED'
             LEFT OUTER JOIN encounter_provider ep ON latestDisposition.encounter_id = ep.encounter_id
             LEFT OUTER JOIN provider disp_provider ON disp_provider.provider_id = ep.provider_id
@@ -62,16 +62,16 @@ FROM bed_location_map blm
             INNER JOIN (SELECT patient_id, MAX(date_started) date_started FROM visit GROUP BY patient_id) visitStartDate on visitStartDate.patient_id = bpam.patient_id
             INNER JOIN visit latestVisit on latestVisit.patient_id = bpam.patient_id and latestVisit.date_started = visitStartDate.date_started
             INNER JOIN encounter on encounter.visit_id = latestVisit.visit_id
-            INNER JOIN obs diagnosis on bpam.patient_id = diagnosis.person_id and diagnosis.encounter_id = encounter.encounter_id
+            INNER JOIN obs diagnosis on bpam.patient_id = diagnosis.person_id and diagnosis.voided = 0 and diagnosis.encounter_id = encounter.encounter_id
                 LEFT OUTER JOIN concept_name diagnosisConceptName on diagnosis.value_coded is not null and diagnosis.value_coded = diagnosisConceptName.concept_id and diagnosisConceptName.concept_name_type='FULLY_SPECIFIED'
                 LEFT OUTER JOIN encounter_provider ep ON diagnosis.encounter_id = ep.encounter_id
                 LEFT OUTER JOIN provider diagnosis_provider ON diagnosis_provider.provider_id = ep.provider_id
                 LEFT OUTER JOIN person_name ON person_name.person_id = diagnosis_provider.person_id
-            INNER JOIN obs certainty on diagnosis.obs_group_id = certainty.obs_group_id and certainty.concept_id = (select concept_id from concept_name where name = 'Diagnosis Certainty' and concept_name_type='FULLY_SPECIFIED')
+            INNER JOIN obs certainty on diagnosis.obs_group_id = certainty.obs_group_id and certainty.voided = 0 and certainty.concept_id = (select concept_id from concept_name where name = 'Diagnosis Certainty' and concept_name_type='FULLY_SPECIFIED')
                 LEFT OUTER JOIN concept_name certaintyConceptName on certainty.value_coded is not null and certainty.value_coded = certaintyConceptName.concept_id and certaintyConceptName.concept_name_type='FULLY_SPECIFIED'
-            INNER JOIN obs diagnosisOrder on diagnosis.obs_group_id = diagnosisOrder.obs_group_id and diagnosisOrder.concept_id = (select concept_id from concept_name where name = 'Diagnosis order' and concept_name_type='FULLY_SPECIFIED')
+            INNER JOIN obs diagnosisOrder on diagnosis.obs_group_id = diagnosisOrder.obs_group_id and diagnosisOrder.voided = 0 and diagnosisOrder.concept_id = (select concept_id from concept_name where name = 'Diagnosis order' and concept_name_type='FULLY_SPECIFIED')
                 LEFT OUTER JOIN concept_name diagnosisOrderConceptName on diagnosisOrder.value_coded is not null and diagnosisOrder.value_coded = diagnosisOrderConceptName.concept_id and diagnosisOrderConceptName.concept_name_type='FULLY_SPECIFIED'
-            INNER JOIN obs diagnosisStatus on diagnosis.obs_group_id = diagnosisStatus.obs_group_id and diagnosisStatus.concept_id = (select concept_id from concept_name where name = 'Bahmni Diagnosis Status' and concept_name_type='FULLY_SPECIFIED')
+            INNER JOIN obs diagnosisStatus on diagnosis.obs_group_id = diagnosisStatus.obs_group_id and diagnosisStatus.voided = 0 and diagnosisStatus.concept_id = (select concept_id from concept_name where name = 'Bahmni Diagnosis Status' and concept_name_type='FULLY_SPECIFIED')
                 LEFT OUTER JOIN concept_name diagnosisStatusConceptName on diagnosisStatus.value_coded is not null and diagnosisStatus.value_coded = diagnosisStatusConceptName.concept_id and diagnosisStatusConceptName.concept_name_type='FULLY_SPECIFIED'
         where bpam.date_stopped is null and diagnosis.concept_id in (select concept_id from concept_name where name in ('Coded Diagnosis', 'Non-Coded Diagnosis') and concept_name_type='FULLY_SPECIFIED')
     ) diagnosis ON diagnosis.person_id = pv.patient_id
