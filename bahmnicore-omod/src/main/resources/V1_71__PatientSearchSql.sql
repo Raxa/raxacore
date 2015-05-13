df3ad9f598c3940f845996939206b6e51c452cc1
abishek91 <abishek91@gmail.com>
2015-05-13 16:39:38 +0530

    Abishek | Removing unneeded statement

--- bahmnicore-omod/src/main/resources/V1_71__PatientSearchSql.sql
+++ bahmnicore-omod/src/main/resources/V1_71__PatientSearchSql.sql
@@ -64,6 +64,3 @@ VALUES ('emrapi.sqlSearch.additionalSearchHandler',
         'Sql query to get list of admitted patients',
         uuid()
 );
-
-
-select distinct concat(pn.given_name,' ', pn.family_name) as name, pi.identifier as identifier, concat("",p.uuid) as uuid, concat("",v.uuid) as activeVisitUuid from encounter e join visit v on e.visit_id = v.visit_id join person_name pn on v.patient_id = pn.person_id and pn.voided = 0 join patient_identifier pi on v.patient_id = pi.patient_id join person p on v.patient_id = p.person_id join encounter_type et on et.encounter_type_id = e.encounter_type where v.date_stopped is null AND v.voided = 0 and et.name = 'ADMISSION' AND e.voided = FALSE AND e.patient_id not in (select distinct enc.patient_id from encounter enc join encounter_type ent on enc.encounter_type = ent.encounter_type_id where ent.name = 'DISCHARGE'  AND enc.voided = FALSE AND enc.patient_id = v.patient_id AND v.visit_id = enc.visit_id)
