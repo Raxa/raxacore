072725b08c24aaaf45d89362f8f6d932acf68d52
Hemanth S.R <hemanthfeb25@gmail.com>
2015-05-14 08:16:37 +0530

    Hemanth | changed the active patients sql to fetch admission status.

--- bahmnicore-omod/src/main/resources/V1_71__PatientSearchSql.sql
+++ bahmnicore-omod/src/main/resources/V1_71__PatientSearchSql.sql
@@ -11,7 +11,20 @@ WHERE property IN (
 
 INSERT INTO global_property (`property`, `property_value`, `description`, `uuid`)
 VALUES ('emrapi.sqlSearch.activePatients',
-        'select distinct concat(pn.given_name,\' \', pn.family_name) as name, pi.identifier as identifier, concat("",p.uuid) as uuid, concat("",v.uuid) as activeVisitUuid from visit v join person_name pn on v.patient_id = pn.person_id and pn.voided = 0 join patient_identifier pi on v.patient_id = pi.patient_id join person p on p.person_id = v.patient_id where v.date_stopped is null AND v.voided = 0',
+        'select distinct
+          concat(pn.given_name,\' \', pn.family_name) as name,
+          pi.identifier as identifier,
+          concat("",p.uuid) as uuid,
+          concat("",v.uuid) as activeVisitUuid,
+          IF(va.value_reference = "Admitted", "true", "false") as hasBeenAdmitted
+        from visit v
+        join person_name pn on v.patient_id = pn.person_id and pn.voided = 0
+        join patient_identifier pi on v.patient_id = pi.patient_id
+        join person p on p.person_id = v.patient_id
+        left outer join visit_attribute va on va.visit_id = v.visit_id and va.visit_attribute_id = (
+          select visit_attribute_type_id from visit_attribute_type where name="Admission Status"
+        )
+        where v.date_stopped is null AND v.voided = 0',
         'Sql query to get list of active patients',
         uuid()
 );
