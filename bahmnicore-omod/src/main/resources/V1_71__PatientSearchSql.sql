c48643dcd7f7c26c965efc677529aea492622928
Hemanth S.R <hemanthfeb25@gmail.com>
2015-05-14 08:35:15 +0530

    Hemanth | changed the patients to Discharge sql to fetch admission status.

--- bahmnicore-omod/src/main/resources/V1_71__PatientSearchSql.sql
+++ bahmnicore-omod/src/main/resources/V1_71__PatientSearchSql.sql
@@ -38,7 +38,32 @@ VALUES ('emrapi.sqlSearch.patientsToAdmit',
 
 INSERT INTO global_property (`property`, `property_value`, `description`, `uuid`)
 VALUES ('emrapi.sqlSearch.patientsToDischarge',
-        'SELECT DISTINCT concat(pn.given_name, \' \', pn.family_name) AS name,pi.identifier AS identifier, concat("", p.uuid) AS uuid, concat("", v.uuid) AS activeVisitUuid FROM visit v INNER JOIN person_name pn ON v.patient_id = pn.person_id and pn.voided is FALSE INNER JOIN patient_identifier pi ON v.patient_id = pi.patient_id and pi.voided is FALSE INNER JOIN person p ON v.patient_id = p.person_id Inner Join (SELECT DISTINCT v.visit_id FROM encounter en INNER JOIN visit v ON v.visit_id = en.visit_id AND en.encounter_type = (SELECT encounter_type_id FROM encounter_type WHERE name = \'ADMISSION\')) v1 on v1.visit_id = v.visit_id INNER JOIN encounter e ON v.visit_id = e.visit_id INNER JOIN obs o ON e.encounter_id = o.encounter_id INNER JOIN concept_name cn ON o.value_coded = cn.concept_id AND cn.concept_name_type = \'FULLY_SPECIFIED\' AND cn.voided is FALSE LEFT OUTER JOIN encounter e1 ON e1.visit_id = v.visit_id AND e1.encounter_type = (SELECT encounter_type_id FROM encounter_type WHERE name = \'DISCHARGE\') AND e1.voided is FALSE WHERE v.date_stopped IS NULL AND v.voided = 0 AND o.voided = 0 AND cn.name = \'Discharge Patient\' AND e1.encounter_id IS NULL',
+        'SELECT DISTINCT
+          concat(pn.given_name, \' \', pn.family_name) AS name,
+          pi.identifier AS identifier,
+          concat("", p.uuid) AS uuid,
+          concat("", v.uuid) AS activeVisitUuid,
+          IF(va.value_reference = "Admitted", "true", "false") as hasBeenAdmitted
+        FROM visit v
+        INNER JOIN person_name pn ON v.patient_id = pn.person_id and pn.voided is FALSE
+        INNER JOIN patient_identifier pi ON v.patient_id = pi.patient_id and pi.voided is FALSE
+        INNER JOIN person p ON v.patient_id = p.person_id
+        Inner Join (SELECT DISTINCT v.visit_id
+          FROM encounter en
+          INNER JOIN visit v ON v.visit_id = en.visit_id AND en.encounter_type =
+            (SELECT encounter_type_id
+              FROM encounter_type
+            WHERE name = "ADMISSION")) v1 on v1.visit_id = v.visit_id
+        INNER JOIN encounter e ON v.visit_id = e.visit_id
+        INNER JOIN obs o ON e.encounter_id = o.encounter_id
+        INNER JOIN concept_name cn ON o.value_coded = cn.concept_id AND cn.concept_name_type = "FULLY_SPECIFIED" AND cn.voided is FALSE
+        left outer join visit_attribute va on va.visit_id = v.visit_id and va.visit_attribute_id =
+          (select visit_attribute_type_id from visit_attribute_type where name="Admission Status")
+        LEFT OUTER JOIN encounter e1 ON e1.visit_id = v.visit_id AND e1.encounter_type = (
+          SELECT encounter_type_id
+            FROM encounter_type
+          WHERE name = "DISCHARGE") AND e1.voided is FALSE
+        WHERE v.date_stopped IS NULL AND v.voided = 0 AND o.voided = 0 AND cn.name = "Discharge Patient" AND e1.encounter_id IS NULL',
         'Sql query to get list of patients to discharge',
         uuid()
 );
