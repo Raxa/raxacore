876c1b728e2ef8ae6895cac043ad4544eff25030
abishek91 <abishek91@gmail.com>
2015-05-18 18:05:33 +0530

    Abishek/Vikash | 2103 | Added data about the person who entered the data on behalf of another provider for diagnosis and drug orders

--- bahmni-emr-api/src/main/java/org/openmrs/module/bahmniemrapi/diagnosis/contract/BahmniDiagnosisRequest.java
+++ bahmni-emr-api/src/main/java/org/openmrs/module/bahmniemrapi/diagnosis/contract/BahmniDiagnosisRequest.java
@@ -1,11 +1,13 @@
 package org.openmrs.module.bahmniemrapi.diagnosis.contract;
 
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.openmrs.PersonName;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BahmniDiagnosisRequest extends BahmniDiagnosis {
     private String previousObs;
     private String encounterUuid;
+    private String personName;
 
     public String getPreviousObs() {
         return previousObs;
@@ -21,5 +23,13 @@ public class BahmniDiagnosisRequest extends BahmniDiagnosis {
 
     public String getEncounterUuid() {
         return encounterUuid;
+    }
+
+    public String getPersonName() {
+        return this.personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
     }
 }
