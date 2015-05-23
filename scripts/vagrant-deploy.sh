ffb92c97e45171da24da672d97dae1e8edd4eac8
mihirk <mihir.khatwani@gmail.com>
2015-05-23 08:41:22 +0530

    Mihir | Adding config service and DAO

--- scripts/vagrant-deploy.sh
+++ scripts/vagrant-deploy.sh
@@ -4,4 +4,4 @@ PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $PATH_OF_CURRENT_SCRIPT/../
 
-mvn clean install -DskipTests -Pvagrant-deploy
+mvn clean install -nsu -DskipTests -Pvagrant-deploy
