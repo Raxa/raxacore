2c6736f93c4c5f76d5ba41b684d08aef796f1d4f
Vikashg <vikashgupta2000@gmail.com>
2015-05-26 18:40:25 +0530

    Vikash | Correcting creatorName from personName.

--- bahmni-emr-api/src/main/java/org/openmrs/module/bahmniemrapi/drugorder/mapper/BahmniDrugOrderMapper.java
+++ bahmni-emr-api/src/main/java/org/openmrs/module/bahmniemrapi/drugorder/mapper/BahmniDrugOrderMapper.java
@@ -33,7 +33,7 @@ public class BahmniDrugOrderMapper {
             bahmniDrugOrder.setDrugOrder(drugOrderMapper.mapDrugOrder(openMRSDrugOrder));
             bahmniDrugOrder.setVisit(openMRSDrugOrder.getEncounter().getVisit());
             bahmniDrugOrder.setProvider(providerMapper.map(openMRSDrugOrder.getOrderer()));
-            bahmniDrugOrder.setPersonName(openMRSDrugOrder.getCreator().getPersonName().toString());
+            bahmniDrugOrder.setCreatorName(openMRSDrugOrder.getCreator().getPersonName().toString());
             bahmniDrugOrders.add(bahmniDrugOrder);
         }
         if(CollectionUtils.isNotEmpty(orderAttributeObs)){
