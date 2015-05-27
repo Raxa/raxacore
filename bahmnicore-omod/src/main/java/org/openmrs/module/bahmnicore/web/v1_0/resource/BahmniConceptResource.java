5bc9e8cf6bf4322ca4a2338781f2314a79325ecf
Preethi <preethis@thoughtworks.com>
2015-05-27 17:19:00 +0530

    Preethi, Abishek | #2086 | Added named representation description for concept answers and removed the hack of ignoring irrelevant fields from DrugResource

--- bahmnicore-omod/src/main/java/org/openmrs/module/bahmnicore/web/v1_0/resource/BahmniConceptResource.java
+++ bahmnicore-omod/src/main/java/org/openmrs/module/bahmnicore/web/v1_0/resource/BahmniConceptResource.java
@@ -4,6 +4,9 @@ import org.openmrs.Concept;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
+import org.openmrs.module.webservices.rest.web.representation.NamedRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.Representation;
+import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
 import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_9.ConceptResource1_9;
 
 @Resource(name = RestConstants.VERSION_1 + "/concept", supportedClass = Concept.class, supportedOpenmrsVersions = {"1.9.*", "1.10.*"}, order = 1)
@@ -30,4 +33,41 @@ public class BahmniConceptResource extends ConceptResource1_9 {
 
         return Context.getConceptService().getConceptByName(uuidOrName);
     }
+
+    @Override
+    public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
+
+        DelegatingResourceDescription representationDescription = super.getRepresentationDescription(rep);
+        if(representationDescription == null){
+            if(rep instanceof NamedRepresentation && rep.getRepresentation().equals("bahmni")){
+                DelegatingResourceDescription description = new DelegatingResourceDescription();
+                description.addProperty("uuid");
+                description.addProperty("name", Representation.DEFAULT);
+                description.addProperty("names", Representation.DEFAULT);
+                description.addProperty("set");
+                description.addProperty("datatype", Representation.DEFAULT);
+                description.addProperty("conceptClass", Representation.DEFAULT);
+                description.addProperty("hiNormal");
+                description.addProperty("lowNormal");
+                description.addProperty("hiAbsolute");
+                description.addProperty("lowAbsolute");
+                description.addProperty("units");
+                description.addProperty("handler");
+                description.addProperty("descriptions", Representation.DEFAULT);
+                description.addProperty("answers", new NamedRepresentation("bahmniAnswer"));
+                description.addProperty("setMembers", new NamedRepresentation("bahmni"));
+                return description;
+            }else if(rep instanceof NamedRepresentation && rep.getRepresentation().equals("bahmniAnswer")){
+                DelegatingResourceDescription description = new DelegatingResourceDescription();
+                description.addProperty("uuid", Representation.DEFAULT);
+                description.addProperty("name", Representation.DEFAULT);
+                description.addProperty("names", Representation.DEFAULT);
+                description.addProperty("displayString");
+                return description;
+            }
+        }
+        return representationDescription;
+    }
+
+
 }
