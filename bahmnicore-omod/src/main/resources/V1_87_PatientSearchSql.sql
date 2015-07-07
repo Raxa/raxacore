393ffb14323a7c82119ea4652a99c47f1362d444
Vikashg <vikashgupta2000@gmail.com>
2015-07-07 13:08:23 +0530

    Revert "Vikash, Chethan | #2468 | Certain patients appearing twice bed and without bed in active patient ."
    This reverts commit 7fc1d7f50f6a210f3316bbb536198316ccfec6ac.

--- bahmnicore-omod/src/main/resources/V1_87_PatientSearchSql.sql
+++ bahmnicore-omod/src/main/resources/V1_87_PatientSearchSql.sql
@@ -1,7 +1,6 @@
 DELETE FROM global_property
 WHERE property IN (
-  'emrapi.sqlSearch.activePatients',
-  'emrapi.sqlSearch.activePatientsByProvider'
+  'emrapi.sqlSearch.activePatients'
 );
 
 INSERT INTO global_property (`property`, `property_value`, `description`, `uuid`)
@@ -22,30 +21,4 @@ VALUES ('emrapi.sqlSearch.activePatients',
         where v.date_stopped is null AND v.voided = 0',
         'Sql query to get list of active patients',
         uuid()
-);
-
-insert into global_property (`property`, `property_value`, `description`, `uuid`)
-values ('emrapi.sqlSearch.activePatientsByProvider','
-  select distinct concat(pn.given_name," ", pn.family_name) as name,
-  pi.identifier as identifier,
-  concat("",p.uuid) as uuid,
-  concat("",v.uuid) as activeVisitUuid,
-  IF(va.value_reference = "Admitted", "true", "false") as hasBeenAdmitted
-  from
-    visit v join person_name pn on v.patient_id = pn.person_id and pn.voided = 0 and v.voided=0
-    join patient_identifier pi on v.patient_id = pi.patient_id and pi.voided=0
-    join person p on p.person_id = v.patient_id  and p.voided=0
-    join encounter en on en.visit_id = v.visit_id and en.voided=0
-    join encounter_provider ep on ep.encounter_id = en.encounter_id  and ep.voided=0
-    join provider pr on ep.provider_id=pr.provider_id and pr.retired=0
-    join person per on pr.person_id=per.person_id and per.voided=0
-    left outer join visit_attribute va on va.visit_id = v.visit_id and va.attribute_type_id = (
-				select visit_attribute_type_id from visit_attribute_type where name="Admission Status"
-			) and va.voided = 0
-  where
-    v.date_stopped is null and
-    pr.uuid=${provider_uuid}
-    order by en.encounter_datetime desc',
-    'Sql query to get list of active patients by provider uuid',
-  uuid()
 );
