7c6755c4f0de8f85829d555320dc1f050803cba3
Venkata Jaswanth <jaswanth@thoughtworks.com>
2015-10-05 22:35:29 +0530

    Jaswanth, Shruthi D | Added automatic lucene index update on bahmni startup

--- bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/Activator.java
+++ bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/Activator.java
@@ -2,18 +2,19 @@ package org.bahmni.module.bahmnicore;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.BaseModuleActivator;
-import org.openmrs.module.ModuleActivator;
 
 public class Activator extends BaseModuleActivator {
-	
+
 	private Log log = LogFactory.getLog(this.getClass());
-	
+
 	@Override
 	public void started() {
 		log.info("Started the Bahmni Core module");
-	}
-	
+        Context.updateSearchIndex();
+    }
+
 	@Override
 	public void stopped() {
 		log.info("Stopped the Bahmni Core module");
