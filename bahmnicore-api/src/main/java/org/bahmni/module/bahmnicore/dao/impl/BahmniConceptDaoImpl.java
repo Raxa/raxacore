c18874fb4ce030e8b92f32cc4dad0d92c378e420
Vinay Venu <vinayv@thoughtworks.com>
2015-11-02 15:18:40 +0530

    Vinay | Enhance search to gracefully handle multiple terms. Voided names will no longer be searched

--- bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/dao/impl/BahmniConceptDaoImpl.java
+++ bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/dao/impl/BahmniConceptDaoImpl.java
@@ -15,24 +15,45 @@ import java.util.List;
 @Repository
 public class BahmniConceptDaoImpl implements BahmniConceptDao {
 
+    private static final String WHITE_SPACE = "\\s+";
+    private static final String WILD_CARD = "%";
+    private static final String BASE_SEARCH_QUERY = "select answer " +
+            "from ConceptAnswer as answer " +
+            "join answer.answerConcept.names  as answerConceptNames " +
+            "where answer.concept = :questionConcept " +
+            " and answerConceptNames.voided = false ";
     @Autowired
     private SessionFactory sessionFactory;
 
     @Override
     public Collection<Concept> searchByQuestion(Concept questionConcept, String searchQuery) {
+        String[] queryArray = searchQuery.split(WHITE_SPACE);
+        StringBuffer queryStringBuffer = new StringBuffer(BASE_SEARCH_QUERY);
+        appendSearchQueriesToBase(queryArray, queryStringBuffer);
+
         Query query = sessionFactory.getCurrentSession().createQuery(
-                "select answer " +
-                        "from ConceptAnswer as answer " +
-                        "join answer.answerConcept.names  as answerConceptNames " +
-                        "where answer.concept = :questionConcept " +
-                        "and lower(answerConceptNames.name) like :searchQuery");
+                queryStringBuffer.toString());
+
         query.setEntity("questionConcept", questionConcept);
-        query.setString("searchQuery", "%" + searchQuery.toLowerCase() + "%");
+        for (int i = 0; i < queryArray.length; i++) {
+            query.setString("query"+ i, searchBothSidesOf(queryArray[i]));
+        }
+
         List<ConceptAnswer> answers = query.list();
         HashSet<Concept> resultConcepts = new HashSet<>();
         for (ConceptAnswer answer : answers) {
             resultConcepts.add(answer.getAnswerConcept());
         }
         return resultConcepts;
+    }
+
+    private void appendSearchQueriesToBase(String[] queryArray, StringBuffer queryStringBuffer) {
+        for (int i = 0; i < queryArray.length; i++) {
+            queryStringBuffer.append(" and lower(answerConceptNames.name) like :query" + i);
+        }
+    }
+
+    private String searchBothSidesOf(String searchString) {
+        return WILD_CARD + searchString.trim().toLowerCase() + WILD_CARD;
     }
 }
