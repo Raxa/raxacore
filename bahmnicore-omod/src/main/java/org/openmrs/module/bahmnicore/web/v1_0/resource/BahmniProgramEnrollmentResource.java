19cdd8e808c1730c618b9ca7dc094dead56893ab
shruthip <shruthi.pitta@gmail.com>
2016-02-01 16:07:45 +0530

    Jaya, Shruthi|#969|Program Management: Values for By and On are not populated ->Modified the program resource so we get custom representation of patient stateintead of REF. ->Added BahmniPatientStateResource to add a property getter for Audit Info

--- bahmnicore-omod/src/main/java/org/openmrs/module/bahmnicore/web/v1_0/resource/BahmniProgramEnrollmentResource.java
+++ bahmnicore-omod/src/main/java/org/openmrs/module/bahmnicore/web/v1_0/resource/BahmniProgramEnrollmentResource.java
@@ -15,6 +15,7 @@ import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.PropertyGetter;
 import org.openmrs.module.webservices.rest.web.annotation.PropertySetter;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
+import org.openmrs.module.webservices.rest.web.representation.CustomRepresentation;
 import org.openmrs.module.webservices.rest.web.representation.DefaultRepresentation;
 import org.openmrs.module.webservices.rest.web.representation.FullRepresentation;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
@@ -65,7 +66,7 @@ public class BahmniProgramEnrollmentResource extends ProgramEnrollmentResource1_
             parentRep.addProperty("attributes", Representation.REF);
             return parentRep;
         } else if (rep instanceof FullRepresentation) {
-            parentRep.addProperty("states", Representation.REF);
+            parentRep.addProperty("states", new CustomRepresentation("(auditInfo,uuid,startDate,endDate,voided,state:REF)"));
             parentRep.addProperty("attributes", Representation.DEFAULT);
             return parentRep;
         } else {
