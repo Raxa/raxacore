96c98c8736f0527b5e992f6171eba3b9e8e55e49
Shireesha <bongaralashireesha@gmail.com>
2016-02-01 10:40:20 +0530

    Shireesha | #980 | Filtering voided program attributes and program states.

--- bahmnicore-omod/src/main/java/org/openmrs/module/bahmnicore/web/v1_0/resource/BahmniProgramEnrollmentResource.java
+++ bahmnicore-omod/src/main/java/org/openmrs/module/bahmnicore/web/v1_0/resource/BahmniProgramEnrollmentResource.java
@@ -4,8 +4,6 @@ package org.openmrs.module.bahmnicore.web.v1_0.resource;
 import org.bahmni.module.bahmnicore.model.bahmniPatientProgram.BahmniPatientProgram;
 import org.bahmni.module.bahmnicore.model.bahmniPatientProgram.PatientProgramAttribute;
 import org.bahmni.module.bahmnicore.service.BahmniProgramWorkflowService;
-import org.openmrs.Location;
-import org.openmrs.LocationAttribute;
 import org.openmrs.Patient;
 import org.openmrs.PatientProgram;
 import org.openmrs.PatientState;
@@ -14,27 +12,20 @@ import org.openmrs.api.PatientService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.webservices.rest.web.RequestContext;
 import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.annotation.PropertyGetter;
 import org.openmrs.module.webservices.rest.web.annotation.PropertySetter;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
 import org.openmrs.module.webservices.rest.web.representation.DefaultRepresentation;
 import org.openmrs.module.webservices.rest.web.representation.FullRepresentation;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.module.webservices.rest.web.resource.api.PageableResult;
-import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
 import org.openmrs.module.webservices.rest.web.resource.impl.EmptySearchResult;
 import org.openmrs.module.webservices.rest.web.resource.impl.NeedsPaging;
 import org.openmrs.module.webservices.rest.web.response.ResponseException;
 import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_10.ProgramEnrollmentResource1_10;
-import org.openmrs.util.OpenmrsUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Resource(name = RestConstants.VERSION_1 + "/programenrollment", supportedClass = BahmniPatientProgram.class, supportedOpenmrsVersions = {"1.12.*,2.*"}, order = 0)
 public class BahmniProgramEnrollmentResource extends ProgramEnrollmentResource1_10 {
@@ -44,6 +35,22 @@ public class BahmniProgramEnrollmentResource extends ProgramEnrollmentResource1_
         for (PatientProgramAttribute attr : attrs) {
             instance.addAttribute(attr);
         }
+    }
+
+    @PropertyGetter("attributes")
+    public static Collection<PatientProgramAttribute> getAttributes(BahmniPatientProgram instance) {
+        return instance.getActiveAttributes();
+    }
+
+    @PropertyGetter("states")
+    public static Collection<PatientState> getStates(BahmniPatientProgram instance) {
+        ArrayList<PatientState> states = new ArrayList<>();
+        for (PatientState state : instance.getStates()) {
+            if(!state.isVoided()){
+                states.add(state);
+            }
+        }
+        return states;
     }
 
     @Override
@@ -111,7 +118,7 @@ public class BahmniProgramEnrollmentResource extends ProgramEnrollmentResource1_
             if(patient == null) {
                 return new EmptySearchResult();
             } else {
-                List patientPrograms = Context.getService(BahmniProgramWorkflowService.class).getPatientPrograms(patient, (Program)null, (Date)null, (Date)null, (Date)null, (Date)null, true);
+                List patientPrograms = Context.getService(BahmniProgramWorkflowService.class).getPatientPrograms(patient, (Program)null, (Date)null, (Date)null, (Date)null, (Date)null, context.getIncludeAll());
                 return new NeedsPaging(patientPrograms, context);
             }
         } else {
