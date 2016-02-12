6ca457bd2af972d7ce162b1eb13d3b153ef0b799
Buddha <sidthunagarajan@gmail.com>
2016-02-12 16:26:48 +0530

    Gautam, Achinta | #1101 | Search specimens based on patient program uuid.
    Added bacteriology omod dependency.
    Introduced Bacteriology sepcimen search handler for searching specimens,based on patientProgram.
    Added baseBacteriology, specimenObs and episodePatientProgramMapping datasets
    Added relevant tests and test data sets.

---
+++ bahmnicore-omod/src/test/java/org/bahmni/module/bahmnicore/web/v1_0/search/BacteriologySpecimenSearchHandlerTest.java
@@ -0,0 +1,74 @@
+package org.bahmni.module.bahmnicore.web.v1_0.search;
+
+import org.bahmni.module.bahmnicore.service.BahmniProgramWorkflowService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.openmrs.Concept;
+import org.openmrs.Encounter;
+import org.openmrs.Obs;
+import org.openmrs.api.ConceptService;
+import org.openmrs.api.ObsService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.bacteriology.api.BacteriologyService;
+import org.openmrs.module.bacteriology.api.encounter.domain.Specimen;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.resource.impl.NeedsPaging;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Context.class)
+public class BacteriologySpecimenSearchHandlerTest {
+
+    @Mock private ConceptService conceptService;
+    @Mock private BahmniProgramWorkflowService bahmniProgramWorkflowService;
+    @Mock private ObsService obsService;
+    @Mock private RequestContext requestContext;
+    @Mock private BacteriologyService bacteriologyService;
+
+    private BacteriologySpecimenSearchHandler bacteriologySpecimenSearchHandler;
+    private final String BACTERIOLOGY_CONCEPT_SET = "BACTERIOLOGY CONCEPT SET";
+
+    @Before
+    public void before() {
+        initMocks(this);
+        when(requestContext.getLimit()).thenReturn(5);
+        PowerMockito.mockStatic(Context.class);
+        when(Context.getService(BacteriologyService.class)).thenReturn(bacteriologyService);
+
+        bacteriologySpecimenSearchHandler = new BacteriologySpecimenSearchHandler(bahmniProgramWorkflowService,
+            conceptService, obsService);
+    }
+
+    @Test
+    public void shouldSearchByPatientProgramUuid() {
+        Concept bacteriologyConceptSet = new Concept();
+        Encounter encounter = new Encounter();
+        Obs observation = new Obs();
+        Specimen specimen = new Specimen();
+
+        List<Encounter> encounters = Arrays.asList(encounter);
+        List<Concept> concepts = Arrays.asList(bacteriologyConceptSet);
+
+        when(requestContext.getParameter("patientProgramUuid")).thenReturn("sample-patientProgramUuid");
+        when(bahmniProgramWorkflowService.getEncountersByPatientProgramUuid("sample-patientProgramUuid")).thenReturn(encounters);
+        when(conceptService.getConceptByName(BACTERIOLOGY_CONCEPT_SET)).thenReturn(bacteriologyConceptSet);
+        when(obsService.getObservations(null, encounters, concepts, null, null, null, null, null, null, null, null, false))
+                .thenReturn(Arrays.asList(observation));
+        when(bacteriologyService.getSpecimenFromObs(observation)).thenReturn(specimen);
+
+        NeedsPaging<Specimen> pageableResult = (NeedsPaging)bacteriologySpecimenSearchHandler.search(requestContext);
+        List<Specimen> resultObservations = pageableResult.getPageOfResults();
+        assertEquals(1, resultObservations.size());
+        assertEquals(resultObservations.get(0), specimen);
+    }
+}
