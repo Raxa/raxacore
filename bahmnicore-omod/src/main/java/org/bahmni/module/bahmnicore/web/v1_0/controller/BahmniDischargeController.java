047684561d0ea5416df44618ce607ca9ebe79407
hemanths <hemanths@ScrewYou.local>
2016-03-07 15:50:27 +0530

    Hemanth | #1157 | endpoint to discharge patient and unassign bed
    Hemanth | #1157 | added dependency on bed management module

---
+++ bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/BahmniDischargeController.java
@@ -0,0 +1,34 @@
+package org.bahmni.module.bahmnicore.web.v1_0.controller;
+
+import org.openmrs.Patient;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.bahmniemrapi.encountertransaction.contract.BahmniEncounterTransaction;
+import org.openmrs.module.bahmniemrapi.encountertransaction.service.BahmniEncounterTransactionService;
+import org.openmrs.module.bedmanagement.BedManagementService;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/bahmnicore/discharge")
+public class BahmniDischargeController {
+
+    @Autowired
+    private BahmniEncounterTransactionService bahmniEncounterTransactionService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    @Transactional
+    public BahmniEncounterTransaction discharge(@RequestBody BahmniEncounterTransaction bahmniEncounterTransaction) {
+        Patient patientByUuid = Context.getPatientService().getPatientByUuid(bahmniEncounterTransaction.getPatientUuid());
+        BedManagementService bedManagementService = (BedManagementService) (Context.getModuleOpenmrsServices(BedManagementService.class.getName()).get(0));
+        bedManagementService.unAssignPatientFromBed(patientByUuid);
+        return bahmniEncounterTransactionService.save(bahmniEncounterTransaction);
+    }
+}
