57d0b096fbfa1c9f75193a3418aad2144413ecc0
Sourav Agarwal <sourava@thoughtworks.com>
2016-03-14 17:43:51 +0530

    Sourav, Abishek | #1059 | New save Patient endpoint to create patient in a single post

---
+++ bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/BahmniPatientProfileResource.java
@@ -0,0 +1,287 @@
+package org.bahmni.module.bahmnicore.web.v1_0.controller;
+
+
+import org.apache.commons.beanutils.ConversionException;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.NonUniqueObjectException;
+import org.openmrs.Patient;
+import org.openmrs.Person;
+import org.openmrs.Relationship;
+import org.openmrs.RelationshipType;
+import org.openmrs.api.ValidationException;
+import org.openmrs.api.context.Context;
+import org.openmrs.api.context.ContextAuthenticationException;
+import org.openmrs.module.emrapi.encounter.DateMapper;
+import org.openmrs.module.emrapi.patient.EmrPatientProfileService;
+import org.openmrs.module.emrapi.patient.PatientProfile;
+import org.openmrs.module.idgen.webservices.services.IdentifierSourceServiceWrapper;
+import org.openmrs.module.webservices.rest.SimpleObject;
+import org.openmrs.module.webservices.rest.web.ConversionUtil;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.api.RestService;
+import org.openmrs.module.webservices.rest.web.representation.Representation;
+import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource;
+import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
+import org.openmrs.module.webservices.rest.web.response.ResourceDoesNotSupportOperationException;
+import org.openmrs.module.webservices.rest.web.response.ResponseException;
+import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_8.PatientResource1_8;
+import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_8.PersonResource1_8;
+import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_8.RelationShipTypeResource1_8;
+import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_8.RelationshipResource1_8;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Controller for REST web service access to
+ * the Search resource.
+ */
+
+@Controller
+@RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/bahmnicore/patientprofile")
+public class BahmniPatientProfileResource extends DelegatingCrudResource<PatientProfile> {
+
+    private EmrPatientProfileService emrPatientProfileService;
+    private IdentifierSourceServiceWrapper identifierSourceServiceWrapper;
+
+    @Autowired
+    public BahmniPatientProfileResource(EmrPatientProfileService emrPatientProfileService, IdentifierSourceServiceWrapper identifierSourceServiceWrapper) {
+        this.emrPatientProfileService = emrPatientProfileService;
+        this.identifierSourceServiceWrapper = identifierSourceServiceWrapper;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<Object> create(@RequestHeader(value = "Jump-Accepted", required = false) boolean jumpAccepted, @RequestBody SimpleObject propertiesToCreate) throws Exception {
+        LinkedHashMap identifierProperties = (LinkedHashMap) ((ArrayList) ((LinkedHashMap) propertiesToCreate.get("patient")).get("identifiers")).get(0);
+        String identifierPrefix = String.valueOf(identifierProperties.get("identifierPrefix"));
+        identifierProperties.remove("identifierPrefix");
+        String identifier;
+        if (identifierProperties.get("registrationNumber") != null) {
+            long givenRegistrationNumber = Long.parseLong(String.valueOf(identifierProperties.get("registrationNumber")));
+            if (!jumpAccepted) {
+                long latestRegistrationNumber = Long.parseLong(identifierSourceServiceWrapper.getSequenceValue(identifierPrefix));
+                long sizeOfJump = givenRegistrationNumber - latestRegistrationNumber;
+                if (sizeOfJump > 0) {
+                    return new ResponseEntity<Object>("{\"sizeOfJump\":" + sizeOfJump + "}", HttpStatus.PRECONDITION_FAILED);
+                } else if (sizeOfJump < 0) {
+                    return new ResponseEntity<Object>("Given identifier is less than the last generated identifier : " + latestRegistrationNumber, HttpStatus.BAD_REQUEST);
+                }
+            }
+            identifier = identifierPrefix + givenRegistrationNumber;
+            identifierSourceServiceWrapper.saveSequenceValue(givenRegistrationNumber + 1, identifierPrefix);
+        } else {
+            identifier = identifierSourceServiceWrapper.generateIdentifier(identifierPrefix, "");
+        }
+        identifierProperties.remove("registrationNumber");
+        identifierProperties.put("identifier", identifier);
+
+        PatientProfile delegate = mapForCreatePatient(propertiesToCreate);
+        setConvertedProperties(delegate, propertiesToCreate, getCreatableProperties(), true);
+        try {
+            delegate = emrPatientProfileService.save(delegate);
+            return new ResponseEntity<>(ConversionUtil.convertToRepresentation(delegate, Representation.FULL), HttpStatus.OK);
+        } catch (Exception e) {
+            if (e instanceof ContextAuthenticationException) {
+                return new ResponseEntity<Object>(e, HttpStatus.FORBIDDEN);
+            } else if (e instanceof NonUniqueObjectException) {
+                return new ResponseEntity<Object>(e.getMessage(), HttpStatus.OK);
+            }  else if (e instanceof ValidationException) {
+                return new ResponseEntity<Object>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            } else {
+                return new ResponseEntity<Object>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/{uuid}")
+    @ResponseBody
+    public ResponseEntity<Object> update(@PathVariable("uuid") String uuid, @RequestBody SimpleObject propertiesToCreate) throws Exception {
+        PatientProfile delegate = mapForUpdatePatient(uuid, propertiesToCreate);
+        setConvertedProperties(delegate, propertiesToCreate, getUpdatableProperties(), true);
+        try {
+            delegate = emrPatientProfileService.save(delegate);
+            return new ResponseEntity<>(ConversionUtil.convertToRepresentation(delegate, Representation.FULL), HttpStatus.OK);
+        } catch (Exception e) {
+            if (e instanceof ContextAuthenticationException) {
+                return new ResponseEntity<Object>(e, HttpStatus.FORBIDDEN);
+            } else if (e instanceof ValidationException) {
+                return new ResponseEntity<Object>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            } else {
+                return new ResponseEntity<Object>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+    }
+
+    private PatientProfile mapForCreatePatient(SimpleObject propertiesToCreate) {
+        final Object patientProperty = propertiesToCreate.get("patient");
+        if (propertiesToCreate.get("patient") == null || !(propertiesToCreate.get("patient") instanceof Map)) {
+            throw new ConversionException("The patient property is missing");
+        }
+
+        PatientProfile delegate = new PatientProfile();
+        PatientResource1_8 patientResource1_9 = (PatientResource1_8) Context.getService(RestService.class).getResourceBySupportedClass(Patient.class);
+        delegate.setPatient(patientResource1_9.getPatient(new SimpleObject() {{
+            putAll((Map<String, Object>) patientProperty);
+        }}));
+        propertiesToCreate.removeProperty("patient");
+        delegate.setRelationships(getRelationships(propertiesToCreate, delegate.getPatient()));
+        propertiesToCreate.removeProperty("relationships");
+        return delegate;
+    }
+
+    private PatientProfile mapForUpdatePatient(String uuid, SimpleObject propertiesToUpdate) {
+        if (propertiesToUpdate.get("patient") == null || !(propertiesToUpdate.get("patient") instanceof Map)) {
+            throw new ConversionException("The patient property is missing");
+        }
+
+        PatientProfile delegate = new PatientProfile();
+
+        PatientResource1_8 patientResource1_9 = (PatientResource1_8) Context.getService(RestService.class).getResourceBySupportedClass(Patient.class);
+        Patient patient = patientResource1_9.getPatientForUpdate(uuid, (Map<String, Object>) propertiesToUpdate.get("patient"));
+        delegate.setPatient(patient);
+
+        propertiesToUpdate.removeProperty("patient");
+        delegate.setRelationships(getRelationships(propertiesToUpdate, delegate.getPatient()));
+
+        return delegate;
+    }
+
+    private List<Relationship> getRelationships(SimpleObject propertiesToCreate, Person currentPerson) {
+        Object relationshipsList = propertiesToCreate.get("relationships");
+        List<Relationship> relationships = new ArrayList<Relationship>();
+        List<Map<String, Object>> relationshipProperties = (List<Map<String, Object>>) relationshipsList;
+        for (final Map<String, Object> relationshipProperty : relationshipProperties) {
+            String uuid = getValueFromMap(relationshipProperty, "uuid");
+            Relationship relationship;
+            if (StringUtils.isBlank(uuid)) {
+                relationship = createRelationship(currentPerson, relationshipProperty);
+            } else {
+                relationship = updateRelationship(relationshipProperty);
+            }
+            relationships.add(relationship);
+        }
+        return relationships;
+    }
+
+    private String getValueFromMap(Map<String, Object> jsonMap, String key) {
+        Object value = jsonMap.get(key);
+        return ObjectUtils.toString(value);
+    }
+
+    private Relationship createRelationship(Person currentPerson, Map<String, Object> relationshipJson) {
+        Relationship relationship = new Relationship(currentPerson,
+                getPerson((Map<String, Object>) relationshipJson.get("personB")),
+                getRelationshipType((Map<String, Object>) relationshipJson.get("relationshipType")));
+        relationship.setEndDate(new DateMapper().convertUTCToDate(getValueFromMap(relationshipJson, "endDate")));
+
+        return relationship;
+    }
+
+    private Person getPerson(Map<String, Object> personJson) {
+        String personUuid = getValueFromMap(personJson, "uuid");
+
+        if (StringUtils.isBlank(personUuid)) {
+            throw new ConversionException("The personUuid is not present.");
+        }
+
+        return getPersonFromUuid(personUuid);
+    }
+
+    private Person getPersonFromUuid(String personUuid) {
+        PersonResource1_8 personResource = (PersonResource1_8) Context.getService(RestService.class).getResourceBySupportedClass(Person.class);
+        Person person = personResource.getByUniqueId(personUuid);
+
+        if (person == null) {
+            throw new ConversionException("The person does not exist.");
+        }
+        return person;
+    }
+
+    private RelationshipType getRelationshipType(Map<String, Object> relationshipTypeJson) {
+
+        String relationshipTypeUuid = getValueFromMap(relationshipTypeJson, "uuid");
+
+        if (StringUtils.isBlank(relationshipTypeUuid)) {
+            throw new ConversionException("The relationshipTypeUuid is not present");
+        }
+
+        RelationShipTypeResource1_8 relationshipResource = (RelationShipTypeResource1_8) Context.getService(RestService.class).getResourceBySupportedClass(RelationshipType.class);
+        RelationshipType relationshipType = relationshipResource.getByUniqueId(relationshipTypeUuid);
+
+        if (relationshipType == null) {
+            throw new ConversionException("The relationship type does not exist.");
+        }
+
+        return relationshipType;
+    }
+
+    private Relationship updateRelationship(final Map<String, Object> relationshipJson) {
+        String relationshipUuid = getValueFromMap(relationshipJson, "uuid");
+
+        if (StringUtils.isBlank(relationshipUuid)) {
+            throw new ConversionException("The relationshipUuid is not present");
+        }
+
+        RelationshipResource1_8 relationshipResource = (RelationshipResource1_8) Context.getService(RestService.class).getResourceBySupportedClass(Relationship.class);
+        Relationship relationship = relationshipResource.getByUniqueId(relationshipUuid);
+
+        if (null == relationship) {
+            throw new ConversionException("Invalid relationship for relationshipUuid " + relationshipUuid);
+        }
+
+        relationshipResource.setConvertedProperties(relationship, relationshipJson, relationshipResource.getUpdatableProperties(), true);
+
+        RelationshipType updatedRelationshipType = getRelationshipType((Map<String, Object>) relationshipJson.get("relationshipType"));
+        relationship.setRelationshipType(updatedRelationshipType);
+
+        return relationship;
+    }
+
+    @Override
+    public PatientProfile getByUniqueId(String s) {
+        return null;
+    }
+
+    @Override
+    protected void delete(PatientProfile patientProfile, String s, RequestContext requestContext) throws ResponseException {
+
+    }
+
+    @Override
+    public PatientProfile newDelegate() {
+        return null;
+    }
+
+    @Override
+    public PatientProfile save(PatientProfile patientProfile) {
+        return null;
+    }
+
+    @Override
+    public void purge(PatientProfile patientProfile, RequestContext requestContext) throws ResponseException {
+
+    }
+
+    public DelegatingResourceDescription getCreatableProperties() throws ResourceDoesNotSupportOperationException {
+        DelegatingResourceDescription description = new DelegatingResourceDescription();
+        description.addProperty("patient", Representation.DEFAULT);
+        description.addProperty("image", Representation.DEFAULT);
+        description.addProperty("relationships", Representation.DEFAULT);
+        return description;
+    }
+
+    @Override
+    public DelegatingResourceDescription getRepresentationDescription(Representation representation) {
+        return null;
+    }
+}
---
+++ bahmnicore-omod/src/test/java/org/bahmni/module/bahmnicore/web/v1_0/controller/BahmniPatientProfileResourceIT.java
@@ -0,0 +1,102 @@
+package org.bahmni.module.bahmnicore.web.v1_0.controller;
+
+import org.apache.commons.io.FileUtils;
+import org.bahmni.module.bahmnicore.web.v1_0.BaseIntegrationTest;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.emrapi.patient.EmrPatientProfileService;
+import org.openmrs.module.emrapi.patient.PatientProfile;
+import org.openmrs.module.idgen.webservices.services.IdentifierSourceServiceWrapper;
+import org.openmrs.module.webservices.rest.SimpleObject;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+import static org.mockito.Mockito.when;
+
+@PrepareForTest(Context.class)
+public class BahmniPatientProfileResourceIT extends BaseIntegrationTest {
+
+    @Autowired
+    private EmrPatientProfileService emrPatientProfileService;
+
+    private BahmniPatientProfileResource bahmniPatientProfileResource;
+    private SimpleObject propertiesToCreate;
+    private ClassLoader classLoader;
+
+    @Mock
+    private IdentifierSourceServiceWrapper identifierSourceServiceWrapper;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        executeDataSet("createPatientMetadata.xml");
+        bahmniPatientProfileResource = new BahmniPatientProfileResource(emrPatientProfileService, identifierSourceServiceWrapper);
+        when(identifierSourceServiceWrapper.getSequenceValue("BAH")).thenReturn("300010");
+        when(identifierSourceServiceWrapper.generateIdentifier("BAH", "")).thenReturn("BAH300010");
+        classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("patient.json").getFile());
+        String jsonString = FileUtils.readFileToString(file);
+        propertiesToCreate = new SimpleObject().parseJson(jsonString);
+    }
+
+    @Test
+    public void shouldReturnHttpPreconditionFailedStatusAndJumpSizeIfIdentifierIsPassedInTheRequestAndTheirIsAJump() throws Exception {
+        LinkedHashMap identifierProperties = (LinkedHashMap) ((ArrayList) ((LinkedHashMap) propertiesToCreate.get("patient")).get("identifiers")).get(0);
+        identifierProperties.put("registrationNumber", "300020");
+        ResponseEntity<Object> response = bahmniPatientProfileResource.create(false, propertiesToCreate);
+        SimpleObject simpleObject = new SimpleObject();
+        simpleObject = simpleObject.parseJson(String.valueOf(response.getBody()));
+        Assert.assertEquals(412, response.getStatusCode().value());
+        Assert.assertEquals(10, Integer.parseInt(String.valueOf(simpleObject.get("sizeOfJump"))));
+    }
+
+    @Test
+    public void shouldCreatePatientWhenUserAcceptsTheJump() throws Exception {
+        LinkedHashMap identifierProperties = (LinkedHashMap) ((ArrayList) ((LinkedHashMap) propertiesToCreate.get("patient")).get("identifiers")).get(0);
+        identifierProperties.put("registrationNumber", "300020");
+        ResponseEntity<Object> response = bahmniPatientProfileResource.create(true, propertiesToCreate);
+        Assert.assertEquals(200, response.getStatusCode().value());
+    }
+
+    @Test
+    public void shouldCreatePatientWhenIdentifierIsPassedAndJumpIsZero() throws Exception {
+        LinkedHashMap identifierProperties = (LinkedHashMap) ((ArrayList) ((LinkedHashMap) propertiesToCreate.get("patient")).get("identifiers")).get(0);
+        identifierProperties.put("registrationNumber", "300010");
+        ResponseEntity<Object> response = bahmniPatientProfileResource.create(false, propertiesToCreate);
+        Assert.assertEquals(200, response.getStatusCode().value());
+    }
+
+    @Test
+    public void shouldCreatePatient() throws Exception {
+        ResponseEntity<Object> response = bahmniPatientProfileResource.create(false, propertiesToCreate);
+        Assert.assertEquals(200, response.getStatusCode().value());
+    }
+
+    @Test
+    public void shouldReturnBadRequestForInvalidJson() throws Exception {
+        LinkedHashMap person = ((LinkedHashMap)((LinkedHashMap)propertiesToCreate.get("patient")).get("person"));
+        person.remove("names");
+        ResponseEntity<Object> response = bahmniPatientProfileResource.create(false, propertiesToCreate);
+        Assert.assertEquals(400, response.getStatusCode().value());
+    }
+
+    @Test
+    public void shouldUpdatePatient() throws Exception {
+        File file = new File(classLoader.getResource("updatePatient.json").getFile());
+        String jsonString = FileUtils.readFileToString(file);
+        propertiesToCreate = new SimpleObject().parseJson(jsonString);
+        String uuid = "592b29e1-b3f5-423e-83cb-0d2c9b80867f";
+        ResponseEntity<Object> response = bahmniPatientProfileResource.update(uuid, propertiesToCreate);
+        Assert.assertEquals(200, response.getStatusCode().value());
+        Assert.assertEquals("Wed Mar 07 00:00:00 IST 1984", ((PatientProfile) response.getBody()).getPatient().getBirthdate().toString());
+    }
+}
