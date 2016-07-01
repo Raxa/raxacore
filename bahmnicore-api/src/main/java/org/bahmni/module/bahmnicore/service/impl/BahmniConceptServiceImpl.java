587f8f44541b5518aa93c688af791e13809cebef
padma <padmavb@thoughtworks.com>
2016-07-01 16:57:08 +0530

    Padma, Alagesan | #1589 | Improving the performance of the visit page

--- bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/service/impl/BahmniConceptServiceImpl.java
+++ bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/service/impl/BahmniConceptServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Component
@@ -57,6 +58,14 @@ public class BahmniConceptServiceImpl implements BahmniConceptService{
     @Transactional(readOnly = true)
     public Concept getConceptByFullySpecifiedName(String drug) {
         return bahmniConceptDao.getConceptByFullySpecifiedName(drug);
+    }
+
+    @Override
+    public List<Concept> getConceptsByFullySpecifiedName(List<String> conceptNames) {
+        if(conceptNames == null || conceptNames.isEmpty()){
+         return Collections.EMPTY_LIST;
+        }
+        return bahmniConceptDao.getConceptsByFullySpecifiedName(conceptNames);
     }
 
     private Concept getConcept(String conceptSetName) {
