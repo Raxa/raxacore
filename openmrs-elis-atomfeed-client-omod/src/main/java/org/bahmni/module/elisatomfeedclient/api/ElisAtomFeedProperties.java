5dc5309431a0e2bb082f15466f0c0f945852c034
Venkata Jaswanth <aj.jaswanth@gmail.com>
2016-10-13 13:17:28 +0530

    Jaswanth | #2374 | Use openelisauthenticator in elis atomfeed client

--- openmrs-elis-atomfeed-client-omod/src/main/java/org/bahmni/module/elisatomfeedclient/api/ElisAtomFeedProperties.java
+++ openmrs-elis-atomfeed-client-omod/src/main/java/org/bahmni/module/elisatomfeedclient/api/ElisAtomFeedProperties.java
@@ -7,18 +7,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class ElisAtomFeedProperties extends AtomFeedProperties {
 
-    private static final String OPEN_ELIS_URI = "openelis.uri";
+    private static final String OPENELIS_URI = "openelis.uri";
     private static final String CONNECT_TIMEOUT = "feed.connectionTimeoutInMilliseconds";
     private static final String MAX_FAILED_EVENTS = "feed.maxFailedEvents";
     private static final String READ_TIMEOUT = "feed.replyTimeoutInMilliseconds";
-    public static final String PATIENT_FEED_URI = "patient.feed.uri";
+    private static final String PATIENT_FEED_URI = "patient.feed.uri";
+    private static final String OPENELIS_USER = "openelis.user";
+    private static final String OPENELIS_PASSWORD = "openelis.password";
 
     public String getPatientFeedUri() {
         return BahmniCoreProperties.getProperty(PATIENT_FEED_URI);
     }
 
+    public String getOpenElisUser() {
+        return BahmniCoreProperties.getProperty(OPENELIS_USER);
+    }
+
+    public String getOpenElisPassword() {
+        return BahmniCoreProperties.getProperty(OPENELIS_PASSWORD);
+    }
+
     public String getOpenElisUri() {
-        return BahmniCoreProperties.getProperty(OPEN_ELIS_URI);
+        return BahmniCoreProperties.getProperty(OPENELIS_URI);
     }
 
     @Override
