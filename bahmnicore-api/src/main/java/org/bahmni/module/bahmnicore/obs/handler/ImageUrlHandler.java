272057f17382ff8cf31e401f616c9d8680c3eba2
Preethi <preethi.ms34@gmail.com>
2016-10-24 14:33:52 +0530

    Preethi | Setting dummy complex data for imageurl handler and video url handler

--- bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/obs/handler/ImageUrlHandler.java
+++ bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/obs/handler/ImageUrlHandler.java
@@ -2,16 +2,19 @@ package org.bahmni.module.bahmnicore.obs.handler;
 
 import org.openmrs.Obs;
 import org.openmrs.api.APIException;
+import org.openmrs.obs.ComplexData;
 import org.openmrs.obs.ComplexObsHandler;
 import org.openmrs.obs.handler.AbstractHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ImageUrlHandler extends AbstractHandler implements ComplexObsHandler {
-    
+
     @Override
     public Obs saveObs(Obs obs) throws APIException {
+        //doing this just to satisfy openmrs obsService.save - it will fail if complex obs does not complex data
+        obs.setComplexData(new ComplexData(obs.getValueComplex(), null));
         return obs;
     }
- 
+
 }
--- bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/obs/handler/VideoUrlHandler.java
+++ bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/obs/handler/VideoUrlHandler.java
@@ -2,6 +2,7 @@ package org.bahmni.module.bahmnicore.obs.handler;
 
 import org.openmrs.Obs;
 import org.openmrs.api.APIException;
+import org.openmrs.obs.ComplexData;
 import org.openmrs.obs.ComplexObsHandler;
 import org.openmrs.obs.handler.AbstractHandler;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,8 @@ public class VideoUrlHandler extends AbstractHandler implements ComplexObsHandle
 
     @Override
     public Obs saveObs(Obs obs) throws APIException {
+        //doing this just to satisfy openmrs obsService.save - it will fail if complex obs does not complex data
+        obs.setComplexData(new ComplexData(obs.getValueComplex(), null));
         return obs;
     }
 
