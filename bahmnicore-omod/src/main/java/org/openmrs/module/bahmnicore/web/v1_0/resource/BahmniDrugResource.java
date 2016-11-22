fc30eaac2b396dd2e9100ad3c2dc3362b61cf7f0
Vinay Venu <vinayv@thoughtworks.com>
2016-11-22 13:09:21 +0530

    Vinay | Ensure we pick up BahmniDrugResource

--- bahmnicore-omod/src/main/java/org/openmrs/module/bahmnicore/web/v1_0/resource/BahmniDrugResource.java
+++ bahmnicore-omod/src/main/java/org/openmrs/module/bahmnicore/web/v1_0/resource/BahmniDrugResource.java
@@ -5,7 +5,8 @@ import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
 import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs2_0.DrugResource2_0;
 
-@org.openmrs.module.webservices.rest.web.annotation.Resource(name = "v1/drug", supportedClass = org.openmrs.Drug.class, supportedOpenmrsVersions = {"1.10.*", "1.11.*", "1.12.*", "2.0.*"}, order = 2)
+@org.openmrs.module.webservices.rest.web.annotation.Resource(name = "v1/drug", supportedClass = org.openmrs.Drug
+        .class, supportedOpenmrsVersions = {"1.10.*", "1.11.*", "1.12.*", "2.0.*"}, order = 0)
 public class BahmniDrugResource extends DrugResource2_0 {
 
 
