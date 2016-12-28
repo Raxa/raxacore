2ef587625429417b90a8b9c20d5bc8dc33df0536
padma <padmavb@thoughtworks.com>
2016-12-28 10:22:17 +0530

    Padma, Pushpa | #2249 - Added globalPropertySearchController for fetching password policies

---
+++ bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/GlobalPropertySearchController.java
@@ -0,0 +1,42 @@
+package org.bahmni.module.bahmnicore.web.v1_0.controller;
+
+import org.openmrs.GlobalProperty;
+import org.openmrs.api.AdministrationService;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Controller
+@RequestMapping(method = RequestMethod.GET, value = "/rest/" + RestConstants.VERSION_1 + "/bahmnicore/globalProperty")
+public class GlobalPropertySearchController extends BaseRestController {
+
+    @Autowired
+    @Qualifier("adminService")
+    AdministrationService administrationService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "passwordPolicyProperties")
+    @ResponseBody
+    public ResponseEntity<HashMap<String, String>> getPasswordPolicies() {
+        List<GlobalProperty> allGlobalProperties = administrationService.getAllGlobalProperties();
+        HashMap<String, String> passwordPolicyProperties = new HashMap<>();
+
+        allGlobalProperties.forEach(globalProperty -> {
+            if (globalProperty.getProperty().contains("security.")) {
+                passwordPolicyProperties.put(globalProperty.getProperty(), globalProperty.getPropertyValue());
+            }
+        });
+
+        return new ResponseEntity<>(passwordPolicyProperties, HttpStatus.OK);
+    }
+
+}
---
+++ bahmnicore-omod/src/test/java/org/bahmni/module/bahmnicore/web/v1_0/controller/GlobalPropertySearchControllerTest.java
@@ -0,0 +1,65 @@
+package org.bahmni.module.bahmnicore.web.v1_0.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.openmrs.GlobalProperty;
+import org.openmrs.api.AdministrationService;
+import org.openmrs.api.context.Context;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@PrepareForTest(Context.class)
+@RunWith(PowerMockRunner.class)
+public class GlobalPropertySearchControllerTest {
+
+    @Mock
+    AdministrationService administrationService;
+
+    @InjectMocks
+    GlobalPropertySearchController globalPropertySearchController;
+
+    private List<GlobalProperty> globalPropertyList = new ArrayList<GlobalProperty>();
+
+
+    @Test
+    public void shouldReturnPasswordRelatedPolicies() throws Exception {
+        GlobalProperty passwordMinLength = new GlobalProperty("security.passwordMinLength","8");
+        GlobalProperty globalProperty = new GlobalProperty("gender","F, M");
+        GlobalProperty passwordCantMatchUserName = new GlobalProperty("security.passwordShouldNotMatchUserName","true");
+
+        globalPropertyList.add(passwordMinLength);
+        globalPropertyList.add(globalProperty);
+        globalPropertyList.add(passwordCantMatchUserName);
+
+        when(administrationService.getAllGlobalProperties()).thenReturn(globalPropertyList);
+
+        ResponseEntity<HashMap<String, String>> passwordPolicies = globalPropertySearchController.getPasswordPolicies();
+
+        assertEquals(2, passwordPolicies.getBody().size());
+        assertEquals("8", passwordPolicies.getBody().get("security.passwordMinLength"));
+        assertEquals("true", passwordPolicies.getBody().get("security.passwordShouldNotMatchUserName"));
+    }
+
+    @Test
+    public void shouldReturnEmptyListIfPasswordPoliciesAreNotThere() throws Exception {
+        GlobalProperty globalProperty = new GlobalProperty("gender","F, M");
+        globalPropertyList.add(globalProperty);
+
+        when(administrationService.getAllGlobalProperties()).thenReturn(globalPropertyList);
+
+        ResponseEntity<HashMap<String, String>> passwordPolicies = globalPropertySearchController.getPasswordPolicies();
+
+        assertEquals(0, passwordPolicies.getBody().size());
+
+    }
+}
