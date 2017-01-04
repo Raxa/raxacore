eacf49cae8abcf6bedc3eaef47559a95aad203bd
pushpa446 <pdumpala@thoughtworks.com>
2017-01-04 17:20:37 +0530

    Pushpa | #2936 | Fix error message for privilege issue

--- bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/BahmniPatientProfileResource.java
+++ bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/BahmniPatientProfileResource.java
@@ -158,7 +158,7 @@ public class BahmniPatientProfileResource extends DelegatingCrudResource<Patient
         try {
             delegate = mapForUpdatePatient(uuid, propertiesToUpdate);
         } catch (APIAuthenticationException e) {
-            return new ResponseEntity<Object>(RestUtil.wrapErrorResponse(e,""), HttpStatus.FORBIDDEN);
+            return new ResponseEntity<Object>(RestUtil.wrapErrorResponse(e, "User is logged in but doesn't have the relevant privilege "), HttpStatus.FORBIDDEN);
         }
         setConvertedProperties(delegate, propertiesToUpdate, getUpdatableProperties(), true);
         delegate.setRelationships(getRelationships(propertiesToUpdate, delegate.getPatient()));
