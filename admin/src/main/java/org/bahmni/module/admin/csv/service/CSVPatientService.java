7fb067cd9357c3fe3927be4c6ebda91a88590738
Rabbia Hassan <rabbia.hassan@ihsinformatics.com>
2019-10-29 19:47:41 +0500

    BAH-677, BAH-679: CSV upload empty Field failure (#61)
    * added check for empty field
    
    * - replaced the nested condition with guard clause.
    - removed whitespaces from the addPersonAttributes function
    
    * removed white-spaces
    
    * removed white spaces.
    
    * white-spaces removed

--- admin/src/main/java/org/bahmni/module/admin/csv/service/CSVPatientService.java
+++ admin/src/main/java/org/bahmni/module/admin/csv/service/CSVPatientService.java
@@ -71,25 +71,27 @@ public class CSVPatientService {
         return patient;
     }
 
-    private void addPersonAttributes(Patient patient, PatientRow patientRow) throws ParseException  {
-        for (KeyValue attribute : patientRow.attributes) {
-            PersonAttributeType personAttributeType = findAttributeType(attribute.getKey());
-            if (personAttributeType.getFormat().equalsIgnoreCase("org.openmrs.Concept")) {
-                Concept concept = getConceptByName(attribute.getValue());
-                if (concept != null) {
-                    patient.addAttribute(new PersonAttribute(personAttributeType, concept.getId().toString()));
-                } else {
-                    throw new RuntimeException("Invalid value for Attribute." + attribute.getKey());
-                }
-            } else if (personAttributeType.getFormat().startsWith("java.lang.")) {
-                patient.addAttribute(new PersonAttribute(findAttributeType(attribute.getKey()), attribute.getValue()));
-           } else if (personAttributeType.getFormat().startsWith("org.openmrs.util.AttributableDate")) {
-                //Validating the Date format
-                String dateString = attribute.getValue();
-                getDateFromString(dateString);
-                patient.addAttribute(new PersonAttribute(findAttributeType(attribute.getKey()),dateString));
-            }
-        }
+    private void addPersonAttributes(Patient patient, PatientRow patientRow) throws ParseException {
+    	for (KeyValue attribute : patientRow.attributes) {
+    		if(StringUtils.isBlank(attribute.getValue()))
+    		   continue;
+    		PersonAttributeType personAttributeType = findAttributeType(attribute.getKey());
+    		if (personAttributeType.getFormat().equalsIgnoreCase("org.openmrs.Concept")) {
+    		    Concept concept = getConceptByName(attribute.getValue());
+    		    if (concept != null) {
+    		    	patient.addAttribute(new PersonAttribute(personAttributeType, concept.getId().toString()));
+    		    } else {
+    		    	throw new RuntimeException("Invalid value for Attribute." + attribute.getKey());
+    		    }
+    		} else if (personAttributeType.getFormat().startsWith("java.lang.")) {
+    		    patient.addAttribute(new PersonAttribute(findAttributeType(attribute.getKey()), attribute.getValue()));
+    		} else if (personAttributeType.getFormat().startsWith("org.openmrs.util.AttributableDate")) {
+    		    //Validating the Date format
+    		    String dateString = attribute.getValue();
+    		    getDateFromString(dateString);
+    		    patient.addAttribute(new PersonAttribute(findAttributeType(attribute.getKey()),dateString));
+    		}
+    	}
     }
 
     private Concept getConceptByName(String name) {
