c49e0a1f604dae1d7391069182e250d7ad887661
Himabindu T <tbindu@thoughtworks.com>
2022-10-14 15:12:29 +0530

    Bindu | BAH-2062 | Fix HTML error response issue with API's (#161)

---
+++ bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/MvcConfiguration.java
@@ -0,0 +1,9 @@
+package org.bahmni.module.bahmnicore.web.v1_0;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@ControllerAdvice
+public class MvcConfiguration {
+}
