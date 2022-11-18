e05e8381da80d63221fdd11ef42ff95669af2049
rohit-yawalkar <82825674+rohit-yawalkar@users.noreply.github.com>
2022-11-18 15:45:14 +0530

    BAH-2486 - [ Rohit ] - Adds AuthorizationAdvice bean to moduleApplicationContext to enable auth security (#166)
    * BAH-2486 - [ Rohit ] - Adds AuthorizationAdvice bean to moduleApplicationContext to enable auth security
    
    * BAH-2486 - Refactors as per feedback

--- bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/service/SqlSearchService.java
+++ bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/service/SqlSearchService.java
@@ -1,5 +1,6 @@
 package org.bahmni.module.bahmnicore.service;
 
+import org.openmrs.annotation.Authorized;
 import org.openmrs.module.webservices.rest.SimpleObject;
 
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.Map;
 
 public interface SqlSearchService {
 
+    @Authorized
     public List<SimpleObject> search(String sqlQuery, Map<String, String[]> params);
 
 }
