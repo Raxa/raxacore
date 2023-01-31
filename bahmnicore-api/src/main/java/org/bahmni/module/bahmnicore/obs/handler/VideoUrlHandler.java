de22634f49a5f47503ad436c8ce63ead3e5d41c3
Himabindu T <tbindu@thoughtworks.com>
2023-01-31 11:51:07 +0530

    Bindu | BAH-2706 | Fix Patient Video FileNotFound exception with complex_obs (#177)

--- bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/obs/handler/VideoUrlHandler.java
+++ bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/obs/handler/VideoUrlHandler.java
@@ -2,15 +2,31 @@ package org.bahmni.module.bahmnicore.obs.handler;
 
 import org.openmrs.Obs;
 import org.openmrs.api.APIException;
+import org.openmrs.obs.ComplexData;
 import org.openmrs.obs.ComplexObsHandler;
 import org.openmrs.obs.handler.AbstractHandler;
+import org.openmrs.obs.handler.BinaryDataHandler;
+import org.openmrs.util.OpenmrsUtil;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
 
 @Component
 public class VideoUrlHandler extends AbstractHandler implements ComplexObsHandler {
 
     @Override
     public Obs saveObs(Obs obs) throws APIException {
+        return obs;
+    }
+
+    @Override
+    public Obs getObs(Obs obs, String view) {
+        File file = BinaryDataHandler.getComplexDataFile(obs);
+        ComplexData complexData = null;
+        complexData = new ComplexData(file.getName(), null);
+        String mimeType = OpenmrsUtil.getFileMimeType(file);
+        complexData.setMimeType(mimeType);
+        obs.setComplexData(complexData);
         return obs;
     }
 
