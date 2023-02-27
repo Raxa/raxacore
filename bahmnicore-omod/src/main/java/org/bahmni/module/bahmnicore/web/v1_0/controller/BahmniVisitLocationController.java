9bbe736725d231ddf98a780cf9fe67c0607fe7ee
kavitha-sundararajan <90255023+kavitha-sundararajan@users.noreply.github.com>
2023-02-27 11:27:23 +0530

    BAH-2863 | add api to get root parent visit location (#182)
    * BAH-2863 | add api to get root parent visit location
    
    * BAH-2863 | refactored as separate endpoints and added testcases
    
    * BAH-2863 | refactor conditions and send minimal data
    
    * BAH-2863 | refactored return types for location

--- bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/BahmniVisitLocationController.java
+++ bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/BahmniVisitLocationController.java
@@ -1,6 +1,8 @@
 package org.bahmni.module.bahmnicore.web.v1_0.controller;
 
 import org.bahmni.module.bahmnicommons.api.visitlocation.BahmniVisitLocationService;
+import org.openmrs.Location;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.HashMap;
 
 @Controller
-@RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/bahmnicore/visitLocation")
+@RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/bahmnicore")
 public class BahmniVisitLocationController extends BaseRestController {
 
     private BahmniVisitLocationService bahmniVisitLocationService;
@@ -24,12 +26,35 @@ public class BahmniVisitLocationController extends BaseRestController {
     }
 
 
-    @RequestMapping(method = RequestMethod.GET, value = "/{loginLocationUuid}")
+    @RequestMapping(method = RequestMethod.GET, value = "/visitLocation/{loginLocationUuid}")
     @ResponseBody
     public HashMap<String, String> getVisitLocationInfo(@PathVariable("loginLocationUuid") String locationUuid ) {
         HashMap<String, String> visitLocation = new HashMap<>();
         visitLocation.put("uuid",bahmniVisitLocationService.getVisitLocationUuid(locationUuid));
         return visitLocation;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/facilityLocation/{locationUuid}")
+    @ResponseBody
+    public HashMap<String, String> getFacilityVisitLocationInfo(@PathVariable("locationUuid") String locationUuid ) {
+        Location location = Context.getLocationService().getLocationByUuid(locationUuid);
+        HashMap<String, String> facilityVisitLocation = new HashMap<>();
+        Location facilityLocation = getParentVisitLocationUuid(location);
+        facilityVisitLocation.put("uuid", facilityLocation!=null ? facilityLocation.getUuid() : null);
+        facilityVisitLocation.put("name", facilityLocation!=null ? facilityLocation.getName() : null);
+        return facilityVisitLocation;
+    }
+
+    private Location getParentVisitLocationUuid(Location location) {
+        if(isVisitLocation(location)) {
+            return location.getParentLocation() != null ? getParentVisitLocationUuid(location.getParentLocation()) : location;
+        } else {
+            return location.getParentLocation() != null ? getParentVisitLocationUuid(location.getParentLocation()) : null;
+        }
+    }
+
+    private Boolean isVisitLocation(Location location) {
+        return (location.getTags().size() > 0 && location.getTags().stream().filter(tag -> tag.getName().equalsIgnoreCase("Visit Location")) != null);
     }
 
 }
--- bahmnicore-omod/src/test/java/org/bahmni/module/bahmnicore/web/v1_0/controller/BahmniVisitLocationControllerIT.java
+++ bahmnicore-omod/src/test/java/org/bahmni/module/bahmnicore/web/v1_0/controller/BahmniVisitLocationControllerIT.java
@@ -8,31 +8,50 @@ import org.bahmni.module.bahmnicommons.api.visitlocation.VisitLocationNotFoundEx
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class BahmniVisitLocationControllerIT extends BaseIntegrationTest {
 
     @Autowired
-    private BahmniVisitLocationController bahmniLocationController;
+    private BahmniVisitLocationController bahmniVisitLocationController;
 
     @Before
     public void setUp() throws Exception {
         executeDataSet("locationData.xml");
     }
 
-        @Test
-        public void shouldGetImmediateParentLocationIdIfItIsTaggedToVisitLocation() throws Exception {
-            String locationUuid = bahmniLocationController.getVisitLocationInfo("c36006e5-9fbb-4f20-866b-0ece245615a1").get("uuid");
-            assertEquals("e36006e5-9fbb-4f20-866b-0ece24561525", locationUuid);
-        }
+    @Test
+    public void shouldGetImmediateParentLocationIdIfItIsTaggedToVisitLocation() throws Exception {
+        String locationUuid = bahmniVisitLocationController.getVisitLocationInfo("c36006e5-9fbb-4f20-866b-0ece245615a1").get("uuid");
+        assertEquals("e36006e5-9fbb-4f20-866b-0ece24561525", locationUuid);
+    }
 
-        @Test
-        public void shouldTraverseTillItGetsParentLocationIdWhichIsTaggedToVisitLocation() throws Exception {
-           String locationUuid= bahmniLocationController.getVisitLocationInfo("e36023e5-9fwb-4f20-866b-0ece24561525").get("uuid");
-            assertEquals("e36006e5-9fbb-4f20-866b-0ece24561525", locationUuid);
-        }
+    @Test
+    public void shouldTraverseTillItGetsParentLocationIdWhichIsTaggedToVisitLocation() throws Exception {
+       String locationUuid= bahmniVisitLocationController.getVisitLocationInfo("e36023e5-9fwb-4f20-866b-0ece24561525").get("uuid");
+        assertEquals("e36006e5-9fbb-4f20-866b-0ece24561525", locationUuid);
+    }
 
-        @Test(expected = VisitLocationNotFoundException.class)
-        public void shouldThrowExceptionIfNoLocationTaggedUntilRoot() throws Exception {
-            bahmniLocationController.getVisitLocationInfo("l36023e5-9fhb-4f20-866b-0ece24561525");
-        }
+    @Test(expected = VisitLocationNotFoundException.class)
+    public void shouldThrowExceptionIfNoLocationTaggedUntilRoot() throws Exception {
+        bahmniVisitLocationController.getVisitLocationInfo("l36023e5-9fhb-4f20-866b-0ece24561525");
+    }
+
+    @Test
+    public void shouldGetRootParentLocationIdWithVisitLocationTag() throws Exception {
+        String locationUuid = bahmniVisitLocationController.getFacilityVisitLocationInfo("g36006e5-9fbb-4f20-866b-0ece24561530").get("uuid");
+        assertEquals("g36006e5-9fbb-4f20-866b-0ece24561528", locationUuid);
+    }
+
+    @Test
+    public void shouldReturnPassedLocationIfParentLocationHasNotVisitLocationTag() throws Exception {
+        String locationUuid = bahmniVisitLocationController.getFacilityVisitLocationInfo("g36006e5-9fbb-4f20-866b-0ece24561527").get("uuid");
+        assertEquals("g36006e5-9fbb-4f20-866b-0ece24561527", locationUuid);
+    }
+
+    @Test
+    public void shouldReturnNullIfLocationHasNotVisitLocationTagTillRoot() throws Exception {
+        String locationUuid = bahmniVisitLocationController.getFacilityVisitLocationInfo("g36006e5-9fbb-4f20-866b-0ece24561531").get("uuid");
+        assertNull(locationUuid);
+    }
 }
